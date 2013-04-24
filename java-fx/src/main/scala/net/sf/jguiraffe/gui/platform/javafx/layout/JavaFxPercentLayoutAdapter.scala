6c22d62f9d78158787e6d5700504788a8ac9e7fa
oheger <oliver.heger@oliver-heger.de>
2013-04-24 19:47:17 +0200

    Added the Java FX-based adapter implementation for PercentLayout.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/layout/JavaFxPercentLayoutAdapter.scala
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.layout
+
+import java.awt.Rectangle
+
+import javafx.geometry.Orientation
+import javafx.scene.Node
+import net.sf.jguiraffe.gui.layout.PercentLayoutPlatformAdapter
+import net.sf.jguiraffe.gui.layout.UnitSizeHandler
+
+/**
+ * The Java FX-based implementation of the ''PercentLayoutPlatformAdapter''
+ * interface.
+ *
+ * This implementation operates on Java FX nodes and provides methods for
+ * querying their sizes and setting their bounds.
+ *
+ * @param components a list with the nodes managed by the associated container
+ * @param constraints an array with constraints for the managed components
+ */
+class JavaFxPercentLayoutAdapter(components: Array[Node],
+  constraints: Array[Object]) extends PercentLayoutPlatformAdapter {
+  /** The number of components managed by the associated container. */
+  val getComponentCount = constraints.size
+
+  /** The size handler used for Java FX percent layouts. */
+  val getSizeHandler = new JavaFxUnitSizeHandler
+
+  def getComponent(index: Int): Object = components(index)
+
+  def getConstraints(index: Int): Object = constraints(index)
+
+  def getMinimumComponentSize(component: Object, vert: Boolean): Int = {
+    val node = asNode(component)
+    getSize(node, vert, node.minWidth(_), node.minHeight(_))
+  }
+
+  def getPreferredComponentSize(component: Object, vert: Boolean): Int = {
+    val node = asNode(component)
+    getSize(node, vert, node.prefWidth(_), node.prefHeight(_))
+  }
+
+  def setComponentBounds(component: Object, bounds: Rectangle) {
+    val node = asNode(component)
+    node.setLayoutX(bounds.x)
+    node.setLayoutY(bounds.y)
+    node.resize(bounds.width, bounds.height)
+  }
+
+  /**
+   * Determines the height of a node based on the passed in functions. This
+   * method takes the node's content bias into account. If necessary, the
+   * node's width is determined first.
+   * @param node the node in question
+   * @param fHeight a function for obtaining the node's height
+   * @param fWidth a function for obtaining the node's width
+   * @return the height of this node
+   */
+  private def getHeight(node: Node, fHeight: Double => Double,
+    fWidth: Double => Double): Int = {
+    val bias = node.getContentBias
+    if (bias == null || bias == Orientation.VERTICAL) {
+      fHeight(-1).toInt
+    } else {
+      val width = fWidth(-1)
+      fHeight(width).toInt
+    }
+  }
+
+  /**
+   * Determines the width of a node based on the passed in functions. This
+   * method takes the node's content bias into account. If necessary, the
+   * node's height is determined first.
+   * @param node the node in question
+   * @param fWidth a function for obtaining the node's width
+   * @param fHeight a function for obtaining the node's height
+   * @return the width of this node
+   */
+  private def getWidth(node: Node, fWidth: Double => Double,
+    fHeight: Double => Double): Int = {
+    val bias = node.getContentBias
+    if (bias == null || bias == Orientation.HORIZONTAL) {
+      fWidth(-1).toInt
+    } else {
+      val height = fHeight(-1)
+      fWidth(height).toInt
+    }
+  }
+
+  /**
+   * Determines the size of a component based on the passed in functions and the
+   * flag for vertical or horizontal.
+   * @param node the node in question
+   * @param vert flag for height or width
+   * @param fWidth the function for querying the width
+   * @param fHeight the function for querying the height
+   * @return the desired size of this component
+   */
+  private def getSize(node: Node, vert: Boolean, fWidth: Double => Double,
+    fHeight: Double => Double): Int = {
+    if (vert) {
+      getHeight(node, fHeight, fWidth)
+    } else {
+      getWidth(node, fWidth, fHeight)
+    }
+  }
+
+  /**
+   * Helper method for casting the component object to a node.
+   * @param component the component to be converted
+   * @return the resulting node object
+   */
+  private def asNode(component: Object): Node = component.asInstanceOf[Node]
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/layout/TestJavaFxPercentLayoutAdapter.scala
@@ -0,0 +1,280 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.layout
+
+import java.awt.Rectangle
+
+import org.easymock.EasyMock
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.api.easymock.PowerMock
+import org.powermock.modules.junit4.PowerMockRunner
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+
+import javafx.geometry.Orientation
+import javafx.scene.Node
+import javafx.scene.text.Text
+
+/**
+ * Test class for ''JavaFxPercentLayoutAdapter''.
+ */
+@RunWith(classOf[PowerMockRunner])
+@PrepareForTest(Array(classOf[Node]))
+class TestJavaFxPercentLayoutAdapter extends JUnitSuite with EasyMockSugar {
+  /** The number of children. */
+  private val ComponentCount = 3
+
+  /** A list with mock nodes representing components. */
+  private var components: Array[Node] = _
+
+  /** An array with test constraints. */
+  private var constraints: Array[Object] = _
+
+  /** The adapter to be tested. */
+  private var adapter: JavaFxPercentLayoutAdapter = _
+
+  @Before def setUp() {
+    components = new Array(ComponentCount)
+    constraints = new Array(ComponentCount)
+    for (i <- 0 until ComponentCount) {
+      components(i) = new Text("Text " + i)
+      constraints(i) = "TestConstraint" + i
+    }
+    adapter = new JavaFxPercentLayoutAdapter(components, constraints)
+  }
+
+  /**
+   * Tests whether the number of components can be determined.
+   */
+  @Test def testGetComponentCount() {
+    assertEquals("Wrong number of components", ComponentCount,
+      adapter.getComponentCount)
+  }
+
+  /**
+   * Tests whether a specific component can be queried.
+   */
+  @Test def testGetComponent() {
+    var idx = 0
+    for (c <- components) {
+      assertEquals("Wrong component at " + idx, c, adapter.getComponent(idx))
+      idx += 1
+    }
+  }
+
+  /**
+   * Tests whether a specific constraints object can be queried.
+   */
+  @Test def testGetConstraints() {
+    var idx = 0
+    for (c <- constraints) {
+      assertEquals("Wrong constraints at " + idx, c, adapter.getConstraints(idx))
+      idx += 1
+    }
+  }
+
+  /**
+   * Creates a mock node with the specified content bias.
+   * @param bias the content bias
+   * @return the mock node
+   */
+  private def nodeWithBias(bias: Orientation): Node = {
+    val node = mock[Node]
+    EasyMock.expect(node.getContentBias).andReturn(bias)
+    node
+  }
+
+  /**
+   * Tests whether the preferred component width can be obtained for nodes with
+   * horizontal bias.
+   */
+  @Test def testGetPreferredComponentWidthHorizontalBias() {
+    val node = nodeWithBias(Orientation.HORIZONTAL)
+    EasyMock.expect(node.prefWidth(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the preferred component width can be obtained for nodes with
+   * no content bias.
+   */
+  @Test def testGetPreferredComponentWidthNoBias() {
+    val node = nodeWithBias(null)
+    EasyMock.expect(node.prefWidth(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the preferred component width can be obtained for nodes with
+   * vertical bias.
+   */
+  @Test def testGetPreferredComponentWidthVerticalBias() {
+    val node = nodeWithBias(Orientation.VERTICAL)
+    val height = 50.0
+    EasyMock.expect(node.prefHeight(-1)).andReturn(height)
+    EasyMock.expect(node.prefWidth(height)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the preferred component height can be obtained for nodes with
+   * vertical bias.
+   */
+  @Test def testGetPreferredComponentHeightVerticalBias() {
+    val node = nodeWithBias(Orientation.VERTICAL)
+    EasyMock.expect(node.prefHeight(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether the preferred component height can be obtained for nodes with
+   * no bias.
+   */
+  @Test def testGetPreferredComponentHeightNoBias() {
+    val node = nodeWithBias(null)
+    EasyMock.expect(node.prefHeight(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether the preferred component height can be obtained for nodes with
+   * horizontal bias.
+   */
+  @Test def testGetPreferredComponentHeightHorizontalBias() {
+    val node = nodeWithBias(Orientation.HORIZONTAL)
+    val width = 150.0
+    EasyMock.expect(node.prefWidth(-1)).andReturn(width)
+    EasyMock.expect(node.prefHeight(width)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getPreferredComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component width can be obtained for nodes with
+   * horizontal bias.
+   */
+  @Test def testGetMinimumComponentWidthHorizontalBias() {
+    val node = nodeWithBias(Orientation.HORIZONTAL)
+    EasyMock.expect(node.minWidth(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component width can be obtained for nodes with
+   * no bias.
+   */
+  @Test def testGetMinimumComponentWidthNoBias() {
+    val node = nodeWithBias(null)
+    EasyMock.expect(node.minWidth(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component width can be obtained for nodes with
+   * vertical bias.
+   */
+  @Test def testGetMinimumComponentWidthVerticalBias() {
+    val node = nodeWithBias(Orientation.VERTICAL)
+    val height = 50.0
+    EasyMock.expect(node.minHeight(-1)).andReturn(height)
+    EasyMock.expect(node.minWidth(height)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, false))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component height can be obtained for nodes with
+   * vertical bias.
+   */
+  @Test def testGetMinimumComponentHeightVerticalBias() {
+    val node = nodeWithBias(Orientation.VERTICAL)
+    EasyMock.expect(node.minHeight(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component height can be obtained for nodes with
+   * no bias.
+   */
+  @Test def testGetMinimumComponentHeightNoBias() {
+    val node = nodeWithBias(null)
+    EasyMock.expect(node.minHeight(-1)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether the minimum component height can be obtained for nodes with
+   * horizontal bias.
+   */
+  @Test def testGetMinimumComponentHeightHorizontalBias() {
+    val node = nodeWithBias(Orientation.HORIZONTAL)
+    val width = 150.0
+    EasyMock.expect(node.minWidth(-1)).andReturn(width)
+    EasyMock.expect(node.minHeight(width)).andReturn(100)
+    whenExecuting(node) {
+      assertEquals("Wrong result", 100, adapter.getMinimumComponentSize(node, true))
+    }
+  }
+
+  /**
+   * Tests whether a component's bounds can be set.
+   */
+  @Test def testSetBounds() {
+    val node = PowerMock.createMock(classOf[Node])
+    val rect = new Rectangle(10, 20, 100, 80)
+    node.setLayoutX(rect.x)
+    node.setLayoutY(rect.y)
+    node.resize(rect.width, rect.height)
+    PowerMock.replayAll()
+    adapter.setComponentBounds(node, rect)
+    PowerMock.verifyAll()
+  }
+
+  /**
+   * Tests the size handler returned by the adapter.
+   */
+  @Test def testGetSizeHandler() {
+    val sizeHandler = adapter.getSizeHandler
+    assertTrue("Wrong size handler", sizeHandler.isInstanceOf[JavaFxUnitSizeHandler])
+    assertSame("Multiple instances", sizeHandler, adapter.getSizeHandler)
+  }
+}
