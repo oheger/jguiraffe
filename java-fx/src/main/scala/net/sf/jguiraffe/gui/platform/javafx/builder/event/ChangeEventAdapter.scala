dd7c922811a4c7c68d9c9935295d6b1da4f2514d
oheger <oliver.heger@oliver-heger.de>
2013-07-23 22:10:06 +0200

    Added new ChangeEventAdapter class.
    This class supports the transformation of Java FX change events to JGUIraffe
    change events.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/event/ChangeEventAdapter.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.event
+
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
+import net.sf.jguiraffe.gui.builder.event.FormChangeEvent
+import net.sf.jguiraffe.gui.builder.event.FormEventManager
+import net.sf.jguiraffe.gui.builder.event.FormListenerType
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+
+/**
+ * An adapter implementation mapping Java FX change notifications to JGUIraffe
+ * [[net.sf.jguiraffe.gui.builder.event.FormChangeEvent]] objects.
+ *
+ * Change events in JGUIraffe are pretty basic. They just transport the
+ * information that the state of a monitored component has changed. An
+ * additional step is necessary to inspect the component's properties and to
+ * find out what actually changed.
+ *
+ * In Java FX component state is typically modeled using properties which
+ * allow the registration of change listeners. Therefore, this adapter
+ * implements a generic change listener. Thus it can be added to an
+ * arbitrary property. Whenever the property changes its value, a corresponding
+ * JGUIraffe change event is generated and passed to the configured sender.
+ * Per default, a sender is used which targets the current ''FormEventManager''
+ * because this is the standard component responsible for the registration of
+ * change listeners.
+ *
+ * @param sender the object for sending events
+ * @param componentHandler the associated component handler
+ * @param componentName the name of the associated component
+ */
+private class ChangeEventAdapter(val sender: EventSender[FormChangeEvent],
+  val componentHandler: ComponentHandler[_], val componentName: String)
+  extends ChangeListener[AnyRef] {
+  /**
+   * Creates a new instance of ''ChangeEventAdapter'' which sends events to the
+   * specified ''FormEventManager'' object.
+   * @param evMan the ''FormEventManager''
+   * @param compHandler the associated component handler
+   * @param compName the name of the associated component
+   */
+  def this(evMan: FormEventManager, compHandler: ComponentHandler[_],
+    compName: String) = this(new EventManagerSender[FormChangeEvent](evMan,
+    FormListenerType.CHANGE), compHandler, compName)
+
+  /**
+   * @inheritdoc This implementation generates a change event and passes it to
+   * the sender.
+   */
+  override def changed(obsValue: ObservableValue[_ <: AnyRef], oldValue: AnyRef,
+    newValue: AnyRef) {
+    sender fire (new FormChangeEvent(obsValue, componentHandler, componentName))
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/event/TestChangeEventAdapter.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.event
+
+import org.junit.Test
+import org.junit.Assert._
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+import net.sf.jguiraffe.gui.builder.event.FormEventManager
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+import net.sf.jguiraffe.gui.builder.event.FormChangeEvent
+import net.sf.jguiraffe.gui.builder.event.FormListenerType
+import javafx.beans.property.StringProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.value.ObservableValue
+
+/**
+ * Test class for ''ChangeEventAdapter''.
+ */
+class TestChangeEventAdapter extends JUnitSuite with EasyMockSugar {
+  /** Constant for a component name. */
+  private val ComponentName = "TestChangeComponent"
+
+  /**
+   * Tests whether a sender is created referencing the current event manager.
+   */
+  @Test def testDefaultSender() {
+    val evMan = mock[FormEventManager]
+    val compHandler = mock[ComponentHandler[_]]
+    val adapter = new ChangeEventAdapter(evMan, compHandler, ComponentName)
+    val sender = adapter.sender.asInstanceOf[EventManagerSender[FormChangeEvent]]
+    assertSame("Wrong event manager", evMan, sender.manager)
+    assertEquals("Wrong listener type", FormListenerType.CHANGE, sender.listenerType)
+  }
+
+  /**
+   * Tests whether a change event is correctly processed.
+   */
+  @Test def testConvertEvent() {
+    val compHandler = mock[ComponentHandler[_]]
+    val sender = new MockSender
+    val obsValue = new SimpleStringProperty
+    val adapter = new ChangeEventAdapter(sender, compHandler, ComponentName)
+    obsValue addListener adapter
+
+    obsValue set "Some value"
+    val event = sender.optEvent.get
+    assertSame("Wrong component handler", compHandler, event.getHandler)
+    assertEquals("Wrong component name", ComponentName, event.getName)
+    assertSame("Wrong source", obsValue, event.getSource)
+  }
+
+  /**
+   * A mock sender for checking the generated change event.
+   */
+  class MockSender extends EventSender[FormChangeEvent] {
+    /** Stores the fired event. */
+    var optEvent: Option[FormChangeEvent] = None
+
+    def fire(event: => FormChangeEvent) {
+      assertFalse("Too many events fired", optEvent.isDefined)
+      optEvent = Some(event)
+    }
+  }
+}
