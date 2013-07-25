2081dfede86df9df5a54cb0236fed81e15706bc0
oheger <oliver.heger@oliver-heger.de>
2013-07-25 21:50:23 +0200

    Added a trait for objects supporting events related to Java FX Node objects.
    This trait can be implemented by ComponentHandler implementations supporting
    basic events (e.g. focus events or mouse events).

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/event/NodeEventSource.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.event
+
+import javafx.scene.Node
+
+/**
+ * A trait for objects supporting the registration of event listeners
+ * for generic node events.
+ *
+ * Java FX ''Node'' objects define a bunch of properties to which event
+ * listeners can be added. This trait enables all of these properties by
+ * just defining a method for returning a ''Node'' object. Event listener
+ * registration code can then access the appropriate properties.
+ */
+trait NodeEventSource {
+  /**
+   * Returns the ''Node'' which is the source of events. At this object
+   * listeners have to be registered.
+   * @return the ''Node'' acting as event source
+   */
+  def sourceNode: Node
+}
