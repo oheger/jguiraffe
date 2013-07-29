4eeafd447f793ec98bbd2daeff92725d1864cb16
oheger <oliver.heger@oliver-heger.de>
2013-07-29 21:59:53 +0200

    Added JavaFxEventManager class.
    This is the Java FX-specific implementation of the PlatformEventManager
    interface. It is responsible for performing event handler registrations for
    JGUIraffe standard events.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/event/JavaFxEventManager.scala
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.event
+
+import org.apache.commons.logging.LogFactory
+
+import javafx.beans.value.ChangeListener
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.scene.input.MouseEvent
+import net.sf.jguiraffe.gui.builder.event.FormEventManager
+import net.sf.jguiraffe.gui.builder.event.FormListenerType
+import net.sf.jguiraffe.gui.builder.event.PlatformEventManager
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+
+/**
+ * The Java FX-specific implementation of the ''PlatformEventManager''
+ * interface.
+ *
+ * This class implements basic functionality for registering default event
+ * listeners at Java FX components. It expects that the passed in
+ * ''ComponentHandler'' objects implement special event source interfaces
+ * for the event types they support. The methods defined by these interfaces
+ * are used to perform the actual listener registration. The listeners used
+ * by this class are special adapter implementations that transform Java FX
+ * events to the generic JGUIraffe event classes.
+ *
+ * Node that this class can only safely be used when collaborating with
+ * [[net.sf.jguiraffe.gui.builder.event.FormEventManager]]. The implementation
+ * relies on certain guarantees given by ''FormEventManager'' regarding
+ * thread-safety or event multiplexing.
+ */
+class JavaFxEventManager extends PlatformEventManager {
+  /** The logger. */
+  private val log = LogFactory.getLog(getClass)
+
+  /** The mapping between listener types and registration objects. */
+  private val registrations = createRegistrationMap()
+
+  /**
+   * @inheritdoc This implementation determines a special handler object for
+   * the registration of the specified listener type and delegates to it.
+   */
+  def registerListener(name: String, handler: ComponentHandler[_],
+    eventManager: FormEventManager, listenerType: FormListenerType) {
+    registrations(listenerType).registerListener(name, handler, eventManager)
+  }
+
+  /**
+   * @inheritdoc This implementation determines a special handler object for
+   * the registration of the specified listener type and delegates to it.
+   */
+  def unregisterListener(name: String, handler: ComponentHandler[_],
+    eventManager: FormEventManager, listenerType: FormListenerType) {
+    registrations(listenerType).unregisterListener(name, handler)
+  }
+
+  /**
+   * Creates a map for obtaining ''ListenerRegistration'' objects for a given
+   * ''FormListenerType''.
+   * @return the map
+   */
+  private def createRegistrationMap(): Map[FormListenerType, ListenerRegistration[_]] =
+    Map(FormListenerType.ACTION -> new ActionListenerRegistration,
+      FormListenerType.CHANGE -> new ChangeListenerRegistration,
+      FormListenerType.FOCUS -> new FocusListenerRegistration,
+      FormListenerType.MOUSE -> new MouseListenerRegistration)
+
+  /**
+   * A base class of a class hierarchy for managing event listener registrations
+   * of a specific type.
+   *
+   * This class already provides base functionality for dealing with event
+   * listeners and for managing registered listeners. Concrete subclasses have
+   * to implement the creation of a corresponding event adapter and the
+   * actual listener registration.
+   *
+   * @tparam L the type of the listeners managed by this class
+   */
+  private abstract class ListenerRegistration[L] {
+    /** The map with registered listeners. */
+    private val listeners = scala.collection.mutable.Map.empty[String, L]
+
+    /**
+     * Performs an event listener registration for the specified component on
+     * behalf of the event manager. This method lets concrete subclasses
+     * perform the registration; the resulting listener is stored so that it
+     * can be removed again later.
+     * @param name the name of the component
+     * @param handler the component handler
+     * @param eventManager the event manager
+     */
+    def registerListener(name: String, handler: ComponentHandler[_],
+      eventManager: FormEventManager) {
+      val optListener = createAndRegisterListener(handler, name, eventManager)
+      optListener foreach (listeners += name -> _)
+    }
+
+    /**
+     * Removes the specified event listener. This method checks whether a
+     * listener has been registered for this component. If so, the
+     * ''removeListener()'' method is called.
+     * @param name the name of the component
+     * @param handler the component handler
+     */
+    def unregisterListener(name: String, handler: ComponentHandler[_]) {
+      listeners.get(name).foreach(removeListener(handler, _))
+    }
+
+    /**
+     * Tries to do a cast from the given component handler to an event source.
+     * This method can be used to find out whether a handler supports a specific
+     * event source interface. An option with the result is returned.
+     * @tparam T the type of the event source to be checked for
+     * @param handler the handler to be checked
+     * @return an ''Option'' with the result of the type cast
+     */
+    protected def eventSource[T](handler: ComponentHandler[_])(implicit m: Manifest[T]): Option[T] = {
+      handler match {
+        case evSrc: T =>
+          Some(evSrc)
+        case _ =>
+          log.warn("Cannot cast " + handler + " to " + m.erasure)
+          None
+      }
+    }
+
+    /**
+     * Creates an event adapter of the supported type and registers it at
+     * the given component handler if possible. If the passed in handler does
+     * not support the expected event listener interface, no registration is
+     * done, and result is ''None''.
+     * @param handler the component handler
+     * @param name the component name
+     * @param eventManager the ''FormEventManager''
+     * @return an ''Option'' with the registered listener
+     */
+    protected def createAndRegisterListener(handler: ComponentHandler[_],
+      name: String, eventManager: FormEventManager): Option[L]
+
+    /**
+     * Removes the given listener from the specified component handler.
+     * This method is called when an event handler is to be unregistered.
+     */
+    protected def removeListener(handler: ComponentHandler[_], l: L)
+  }
+
+  /**
+   * A concrete ''ListenerRegistration'' class responsible for action listeners.
+   */
+  private class ActionListenerRegistration
+    extends ListenerRegistration[EventHandler[ActionEvent]] {
+    protected def createAndRegisterListener(handler: ComponentHandler[_],
+      name: String, eventManager: FormEventManager): Option[EventHandler[ActionEvent]] = {
+      val source = eventSource[ActionEventSource](handler)
+      source map { s =>
+        val adapter = new ActionEventAdapter(eventManager, handler, name, null)
+        s.addActionListener(adapter)
+        adapter
+      }
+    }
+
+    protected def removeListener(handler: ComponentHandler[_],
+      l: EventHandler[ActionEvent]) {
+      eventSource[ActionEventSource](handler) foreach (_.removeActionListener(l))
+    }
+  }
+
+  /**
+   * A concrete ''ListenerRegistration'' class responsible for change listeners.
+   */
+  private class ChangeListenerRegistration
+    extends ListenerRegistration[ChangeListener[AnyRef]] {
+    protected def createAndRegisterListener(handler: ComponentHandler[_],
+      name: String, eventManager: FormEventManager): Option[ChangeListener[AnyRef]] = {
+      val source = eventSource[ChangeEventSource](handler)
+      source map { s =>
+        val adapter = new ChangeEventAdapter(eventManager, handler, name)
+        s.observableValue.addListener(adapter)
+        adapter
+      }
+    }
+
+    protected def removeListener(handler: ComponentHandler[_],
+      l: ChangeListener[AnyRef]) {
+      val source = eventSource[ChangeEventSource](handler)
+      source foreach (_.observableValue.removeListener(l))
+    }
+  }
+
+  /**
+   * A concrete ''ListenerRegistration'' class responsible for focus listeners.
+   */
+  private class FocusListenerRegistration
+    extends ListenerRegistration[FocusEventAdapter] {
+    protected def createAndRegisterListener(handler: ComponentHandler[_],
+      name: String, eventManager: FormEventManager): Option[FocusEventAdapter] = {
+      val source = eventSource[NodeEventSource](handler)
+      source map { s =>
+        val adapter = new FocusEventAdapter(eventManager, handler, name)
+        adapter register s.sourceNode
+        adapter
+      }
+    }
+
+    protected def removeListener(handler: ComponentHandler[_],
+      adapter: FocusEventAdapter) {
+      val source = eventSource[NodeEventSource](handler)
+      source foreach (s => adapter.unregister(s.sourceNode))
+    }
+  }
+
+  /**
+   * A concrete ''ListenerRegistration'' class responsible for mouse listeners.
+   */
+  private class MouseListenerRegistration
+    extends ListenerRegistration[EventHandler[MouseEvent]] {
+    protected def createAndRegisterListener(handler: ComponentHandler[_],
+      name: String, eventManager: FormEventManager): Option[EventHandler[MouseEvent]] = {
+      val source = eventSource[NodeEventSource](handler)
+      source map { s =>
+        val adapter = MouseEventAdapter(eventManager, handler, name)
+        s.sourceNode.addEventHandler(MouseEvent.ANY, adapter)
+        adapter
+      }
+    }
+
+    protected def removeListener(handler: ComponentHandler[_],
+      l: EventHandler[MouseEvent]) {
+      val source = eventSource[NodeEventSource](handler)
+      source foreach (_.sourceNode.removeEventHandler(MouseEvent.ANY, l))
+    }
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/event/TestJavaFxEventManager.scala
@@ -0,0 +1,258 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.event
+
+import org.easymock.EasyMock
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertSame
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.api.easymock.PowerMock
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import org.scalatest.junit.JUnitSuite
+import javafx.beans.property.ReadOnlyBooleanProperty
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.scene.Node
+import javafx.scene.input.MouseEvent
+import net.sf.jguiraffe.gui.builder.event.FormEventManager
+import net.sf.jguiraffe.gui.builder.event.FormFocusEvent
+import net.sf.jguiraffe.gui.builder.event.FormListenerType
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+import net.sf.jguiraffe.gui.platform.javafx.FetchAnswer
+import net.sf.jguiraffe.gui.platform.javafx.FetchAnswer.convertToOption
+
+/**
+ * Test class for ''JavaFxEventManager''.
+ */
+@RunWith(classOf[PowerMockRunner])
+@PrepareForTest(Array(classOf[Node]))
+class TestJavaFxEventManager extends JUnitSuite {
+  /** Constant for a test component name. */
+  private val CompName = "JavaFxEventManagerTestComponent"
+
+  /** A mock for the form event manager. */
+  private var formEvMan: FormEventManager = _
+
+  /** The event manager to be tested. */
+  private var evMan: JavaFxEventManager = _
+
+  @Before def setUp() {
+    formEvMan = PowerMock.createMock(classOf[FormEventManager])
+    evMan = new JavaFxEventManager
+  }
+
+  /**
+   * Checks whether an event sender is correctly configured.
+   * @param sender the sender
+   * @param lType the expected listener type
+   */
+  private def checkSender(sender: EventSender[_], lType: FormListenerType) {
+    sender match {
+      case evManSender: EventManagerSender[_] =>
+        assertSame("Wrong event manager", formEvMan, evManSender.manager)
+        assertEquals("Wrong listener type", lType, evManSender.listenerType)
+      case _ =>
+        fail("Unexpected sender: " + sender)
+    }
+  }
+
+  /**
+   * Performs registration checks for a component handler and a given listener
+   * type. A listener is registered and unregistered.
+   * @param compHandler the ''ComponentHandler''
+   * @param lType the listener type
+   */
+  private def doRegistration(compHandler: ComponentHandler[_],
+    lType: FormListenerType) {
+    evMan.registerListener(CompName, compHandler, formEvMan, lType)
+    evMan.unregisterListener(CompName, compHandler, formEvMan, lType)
+  }
+
+  /**
+   * Tests whether action listeners are handled correctly.
+   */
+  @Test def testActionListenerRegistration() {
+    val compHandler = PowerMock.createMock(classOf[CompHandlerWithActionSource])
+    val aRegister = new FetchAnswer[AnyRef, EventHandler[ActionEvent]]
+    val aUnRegister = new FetchAnswer[AnyRef, EventHandler[ActionEvent]]
+    compHandler.addActionListener(
+      EasyMock.anyObject(classOf[EventHandler[ActionEvent]]))
+    EasyMock.expectLastCall().andAnswer(aRegister)
+    compHandler.removeActionListener(
+      EasyMock.anyObject(classOf[EventHandler[ActionEvent]]))
+    EasyMock.expectLastCall().andAnswer(aUnRegister)
+    PowerMock.replayAll()
+
+    doRegistration(compHandler, FormListenerType.ACTION)
+    val listener = aRegister.get.asInstanceOf[ActionEventAdapter]
+    checkSender(listener.sender, FormListenerType.ACTION)
+    assertSame("Wrong component handler", compHandler, listener.componentHandler)
+    assertEquals("Wrong component name", CompName, listener.componentName)
+    assertSame("Wrong object unregistered", listener, aUnRegister.get)
+    PowerMock.verifyAll()
+  }
+
+  /**
+   * Tests whether listener registration handles the case that the desired
+   * listener interface is not supported. The registration should be ignored.
+   */
+  @Test def testActionListenerNoActionSource() {
+    val compHandler = PowerMock.createMock(classOf[ComponentHandler[AnyRef]])
+    PowerMock.replayAll()
+
+    evMan.registerListener(CompName, compHandler, formEvMan,
+      FormListenerType.ACTION)
+  }
+
+  /**
+   * Tests that a request to unregister an unknown listener is ignored.
+   */
+  @Test def testUnregisterUnknownListener() {
+    val compHandler = PowerMock.createMock(classOf[CompHandlerWithActionSource])
+    PowerMock.replayAll()
+
+    evMan.unregisterListener(CompName, compHandler, formEvMan,
+      FormListenerType.ACTION)
+  }
+
+  /**
+   * Tests whether change listeners are handled correctly.
+   */
+  @Test def testChangeListenerRegistration() {
+    val compHandler = PowerMock.createMock(classOf[CompHandlerWithChangeSource])
+    val obsValue = PowerMock.createMock(classOf[ObservableValue[AnyRef]])
+    compHandler.observableValue
+    EasyMock.expectLastCall().andReturn(obsValue).anyTimes()
+    val aRegister = new FetchAnswer[AnyRef, ChangeListener[AnyRef]]
+    val aUnregister = new FetchAnswer[AnyRef, ChangeListener[AnyRef]]
+    obsValue.addListener(EasyMock.anyObject(classOf[ChangeListener[AnyRef]]))
+    EasyMock.expectLastCall().andAnswer(aRegister)
+    obsValue.removeListener(EasyMock.anyObject(classOf[ChangeListener[AnyRef]]))
+    EasyMock.expectLastCall().andAnswer(aUnregister)
+    PowerMock.replayAll()
+
+    doRegistration(compHandler, FormListenerType.CHANGE)
+    val listener = aRegister.get.asInstanceOf[ChangeEventAdapter]
+    assertSame("Wrong component handler", compHandler, listener.componentHandler)
+    assertEquals("Wrong component name", CompName, listener.componentName)
+    checkSender(listener.sender, FormListenerType.CHANGE)
+    assertSame("Wrong unregistered listener", listener, aUnregister.get)
+    PowerMock.verifyAll()
+  }
+
+  /**
+   * Tests whether focus listeners are handled correctly.
+   */
+  @Test def testFocusListenerRegistration() {
+    /**
+     * Helper method for testing a received event.
+     * @param ans the answer with the event
+     * @param handler the expected handler
+     * @param expType the expected event type
+     */
+    def checkFocusEvent(ans: FetchAnswer[_, FormFocusEvent],
+      handler: ComponentHandler[_], expType: FormFocusEvent.Type) {
+      assertSame("Wrong handler", handler, ans.get.getHandler)
+      assertEquals("Wrong component name", CompName, ans.get.getName)
+      assertEquals("Wrong event type", expType, ans.get.getType)
+    }
+
+    val compHandler = PowerMock.createMock(classOf[CompHandlerWithNodeSource])
+    val node = PowerMock.createMock(classOf[Node])
+    val focusProp = PowerMock.createMock(classOf[ReadOnlyBooleanProperty])
+    EasyMock.expect(compHandler.sourceNode).andReturn(node).anyTimes()
+    EasyMock.expect(node.focusedProperty).andReturn(focusProp).anyTimes()
+    val aRegister = new FetchAnswer[AnyRef, ChangeListener[java.lang.Boolean]]
+    val aUnregister = new FetchAnswer[AnyRef, ChangeListener[java.lang.Boolean]]
+    focusProp.addListener(EasyMock.anyObject(classOf[ChangeListener[AnyRef]]))
+    EasyMock.expectLastCall().andAnswer(aRegister)
+    focusProp.removeListener(EasyMock.anyObject(classOf[ChangeListener[AnyRef]]))
+    EasyMock.expectLastCall().andAnswer(aUnregister)
+    val aFocusGainedEvent = new FetchAnswer[AnyRef, FormFocusEvent]
+    val aFocusLostEvent = new FetchAnswer[AnyRef, FormFocusEvent]
+    formEvMan.fireEvent(EasyMock.anyObject(classOf[FormFocusEvent]),
+      EasyMock.eq(FormListenerType.FOCUS))
+    EasyMock.expectLastCall().andAnswer(aFocusGainedEvent)
+    formEvMan.fireEvent(EasyMock.anyObject(classOf[FormFocusEvent]),
+      EasyMock.eq(FormListenerType.FOCUS))
+    EasyMock.expectLastCall().andAnswer(aFocusLostEvent)
+    PowerMock.replayAll()
+
+    doRegistration(compHandler, FormListenerType.FOCUS)
+    val listener = aRegister.get
+    listener.changed(focusProp, java.lang.Boolean.FALSE, java.lang.Boolean.TRUE)
+    listener.changed(focusProp, java.lang.Boolean.TRUE, java.lang.Boolean.FALSE)
+
+    checkFocusEvent(aFocusGainedEvent, compHandler,
+      FormFocusEvent.Type.FOCUS_GAINED)
+    checkFocusEvent(aFocusLostEvent, compHandler,
+      FormFocusEvent.Type.FOCUS_LOST)
+    assertSame("Wrong unregistered listener", listener, aUnregister.get)
+    PowerMock.verifyAll()
+  }
+
+  /**
+   * Tests whether mouse listeners are handled correctly.
+   */
+  @Test def testMouseListenerRegistration() {
+    val compHandler = PowerMock.createMock(classOf[CompHandlerWithNodeSource])
+    val node = PowerMock.createMock(classOf[Node])
+    EasyMock.expect(compHandler.sourceNode).andReturn(node).anyTimes()
+    val aRegister = new FetchAnswer[AnyRef, EventHandler[MouseEvent]](argIdx = 1)
+    val aUnregister = new FetchAnswer[AnyRef, EventHandler[MouseEvent]](argIdx = 1)
+    node.addEventHandler(EasyMock.eq(MouseEvent.ANY),
+      EasyMock.anyObject(classOf[EventHandler[MouseEvent]]))
+    EasyMock.expectLastCall().andAnswer(aRegister)
+    node.removeEventHandler(EasyMock.eq(MouseEvent.ANY),
+      EasyMock.anyObject(classOf[EventHandler[MouseEvent]]))
+    EasyMock.expectLastCall().andAnswer(aUnregister)
+    PowerMock.replayAll()
+
+    doRegistration(compHandler, FormListenerType.MOUSE)
+    val listener = aRegister.get.asInstanceOf[MouseEventAdapter]
+    assertSame("Wrong component handler", compHandler, listener.componentHandler)
+    assertEquals("Wrong component name", CompName, listener.componentName)
+    checkSender(listener.sender, FormListenerType.MOUSE)
+    assertSame("Wrong unregistered listener", listener, aUnregister.get)
+    PowerMock.verifyAll()
+  }
+}
+
+/**
+ * A combined trait for a component handler and an action event source needed
+ * for mock creation.
+ */
+private trait CompHandlerWithActionSource
+  extends ComponentHandler[AnyRef] with ActionEventSource
+
+/**
+ * A combined trait for a component handler and a change event source needed
+ * for mock creation.
+ */
+private trait CompHandlerWithChangeSource
+  extends ComponentHandler[AnyRef] with ChangeEventSource
+
+/**
+ * A combined trait for a component handler and a node event source needed
+ * for mock creation.
+ */
+private trait CompHandlerWithNodeSource
+  extends ComponentHandler[AnyRef] with NodeEventSource
