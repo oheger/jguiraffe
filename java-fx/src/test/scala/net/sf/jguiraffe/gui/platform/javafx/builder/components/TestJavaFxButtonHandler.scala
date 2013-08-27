2276fe30704ddb843de61b0bbe03bf2795727bf8
oheger <oliver.heger@oliver-heger.de>
2013-08-27 21:42:58 +0200

    Added new JavaFxButtonHandler class.
    This class implements the ComponentHandler interface for JavaFX buttons. It
    also serves as a base class for similar handlers, e.g. for checkboxes or
    toggle buttons.

---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/TestJavaFxButtonHandler.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+
+import javafx.scene.control.ButtonBase
+
+/**
+ * Test class for ''JavaFxButtonHandler''.
+ */
+class TestJavaFxButtonHandler extends JUnitSuite with EasyMockSugar {
+  /** The mock for the button control. */
+  private var button: ButtonBase = _
+
+  /** The handler to be tested. */
+  private var handler: JavaFxButtonHandler = _
+
+  @Before def setUp() {
+    button = mock[ButtonBase]
+    handler = new JavaFxButtonHandler(button)
+  }
+
+  /**
+   * Tests whether the handler supports adding action listeners.
+   */
+  @Test def testActionListenerSupport() {
+    assertTrue("No control action listener support",
+      handler.isInstanceOf[ControlActionEventSource[java.lang.Boolean]])
+  }
+
+  /**
+   * Tests whether the correct data type is returned.
+   */
+  @Test def testGetType() {
+    assertEquals("Wrong data type", classOf[java.lang.Boolean], handler.getType)
+  }
+
+  /**
+   * Tests the dummy getData() implementation. Here we can only test that the
+   * button control is not manipulated.
+   */
+  @Test def testGetData() {
+    whenExecuting(button) {
+      assertEquals("Wrong result", java.lang.Boolean.FALSE, handler.getData)
+    }
+  }
+
+  /**
+   * Tests the dummy setData() implementation. Here we can only test that the
+   * button control is not touched.
+   */
+  @Test def testSetData() {
+    whenExecuting(button) {
+      handler setData java.lang.Boolean.TRUE
+    }
+  }
+}
