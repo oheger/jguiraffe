e1915e5d33a2dd4ad254ca462986daa6d8eff5e7
oheger <oliver.heger@oliver-heger.de>
2013-10-11 22:10:09 +0200

    Added a ComponentHandler implementation for lists with single selection.
    The handler class wraps a JavaFX ListView control in single selection mode.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/JavaFxListViewHandler.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import javafx.scene.control.ListView
+import net.sf.jguiraffe.gui.builder.components.tags.ListModelUtils
+import net.sf.jguiraffe.gui.platform.javafx.builder.event.ChangeEventSource
+
+/**
+ * A specialized ''ComponentHandler'' implementation for JavaFX list view
+ * components.
+ *
+ * This class manages a ''ListView'' control in single selection mode.
+ * Its data is of type ''Object'', i.e. the object from the list model
+ * whose index is selected in the ''ListView'' control. Change events are
+ * supported and mapped to selection changes.
+ *
+ * @param listView the ''ListView'' control to be managed
+ */
+private class JavaFxListViewHandler(listView: ListView[Object])
+  extends JavaFxComponentHandler[Object](listView) with ListModelSupport
+  with ChangeEventSource {
+  protected val displayList = listView.getItems
+
+  override val observableValue = listView.getSelectionModel.selectedIndexProperty
+
+  /**
+   * @inheritdoc This implementation obtains the selected index from the
+   * managed list box. Then the corresponding value object from the list
+   * model is returned.
+   */
+  def getData: Object = ListModelUtils.getValue(getListModel,
+    listView.getSelectionModel.getSelectedIndex)
+
+  /**
+   * @inheritdoc This implementation determines the index of the passed in
+   * object. This index - if defined - is then set as selected index.
+   */
+  def setData(data: Object) {
+    listView.getSelectionModel.clearSelection()
+
+    if (data != null) {
+      val idx = ListModelUtils.getIndex(getListModel, data)
+      if (idx != ListModelUtils.IDX_UNDEFINED) {
+        listView.getSelectionModel.select(idx)
+      }
+    }
+  }
+
+  /**
+   * @inheritdoc This implementation returns the type from the list model.
+   */
+  def getType: Class[_] = getListModel.getType
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/TestJavaFxListViewHandler.scala
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+
+import javafx.scene.control.ListView
+import net.sf.jguiraffe.gui.builder.components.model.ListModel
+
+/**
+ * Test class for ''JavaFxListViewHandler''.
+ */
+class TestJavaFxListViewHandler extends JUnitSuite {
+  /** The list view control to be managed by the test instance. */
+  private var listView: ListView[Object] = _
+
+  /** The list model used by the handler. */
+  private var listModel: ListModel = _
+
+  /** The handler to be tested. */
+  private var handler: JavaFxListViewHandler = _
+
+  @Before def setUp() {
+    listView = new ListView
+    listModel = new ListModelTestImpl
+    handler = new JavaFxListViewHandler(listView)
+    handler initListModel listModel
+  }
+
+  /**
+   * Checks that the test list has no selection.
+   */
+  private def checkNoSelection() {
+    assertTrue("Got a selection",
+      listView.getSelectionModel.getSelectedIndices.isEmpty);
+    assertNull("Got a selected item", listView.getSelectionModel.getSelectedItem)
+  }
+
+  /**
+   * Tests whether the correct display list is returned.
+   */
+  @Test def testDisplayList() {
+    val model = handler.getListModel.asInstanceOf[JavaFxListModel]
+    assertSame("Wrong display list", listView.getItems, model.displayList)
+  }
+
+  /**
+   * Tests whether the correct type is returned.
+   */
+  @Test def testGetType() {
+    assertEquals("Wrong type", listModel.getType, handler.getType)
+  }
+
+  /**
+   * Tests setData() for null input.
+   */
+  @Test def testSetDataNull() {
+    listView.getSelectionModel.selectAll()
+    handler setData null
+    checkNoSelection()
+  }
+
+  /**
+   * Tests setData() if a model object is passed in.
+   */
+  @Test def testSetDataModelObject() {
+    val idx = 4
+    handler setData listModel.getValueObject(idx)
+    assertEquals("Wrong selected index", idx,
+      listView.getSelectionModel.getSelectedIndex)
+  }
+
+  /**
+   * Tests setData() if an unknown object is passed in.
+   */
+  @Test def testSetDataModelOtherObject() {
+    listView.getSelectionModel.selectAll()
+    handler setData "someUnknownObject"
+    checkNoSelection()
+  }
+
+  /**
+   * Tests getData() if there is no selection.
+   */
+  @Test def testGetDataNoSelection() {
+    listView.getSelectionModel.clearSelection()
+    assertNull("Got data", handler.getData)
+  }
+
+  /**
+   * Tests whether the selected item can be obtained.
+   */
+  @Test def testGetDataSelection() {
+    val idx = 2
+    listView.getSelectionModel.select(idx)
+    assertEquals("Wrong data", listModel.getValueObject(idx), handler.getData)
+  }
+
+  /**
+   * Tests whether the expected property for change events is returned.
+   */
+  @Test def testChangeEventSource() {
+    assertSame("Wrong observable value",
+      listView.getSelectionModel.selectedIndexProperty, handler.observableValue)
+  }
+}
