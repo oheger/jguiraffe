921a8219ece59e3c58e23da3614767529194a3df
oheger <oliver.heger@oliver-heger.de>
2013-12-16 21:56:55 +0100

    Added ConfigNodeData data class.
    This class is used to represent a tree node. It is a thin wrapper around a
    ConfigurationNode - which actually contains a tree node's data - with a
    toString() method defining the display of the node in the tree view.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/tree/ConfigNodeData.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.tree
+
+import org.apache.commons.configuration.tree.ConfigurationNode
+
+/**
+ * A simple data class representing information about a tree node.
+ *
+ * This class is just a thin wrapper around a ''ConfigurationNode'' which
+ * actually contains the data about a tree node. It defines a ''toString()''
+ * method to construct the string which is displayed by the tree cell.
+ *
+ * @param node the underlying ''ConfigurationNode''
+ */
+private case class ConfigNodeData(node: ConfigurationNode) {
+  override def toString = node.getName
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/tree/TestConfigNodeData.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.tree
+
+import org.apache.commons.configuration.tree.ConfigurationNode
+import org.easymock.EasyMock
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+
+/**
+ * Test class for ''ConfigNodeData''.
+ */
+class TestConfigNodeData extends JUnitSuite with EasyMockSugar {
+  /**
+   * Tests the string representation of a node object.
+   */
+  @Test def testToString() {
+    val node = mock[ConfigurationNode]
+    val NodeName = "MyTestNode"
+    EasyMock.expect(node.getName).andReturn(NodeName).anyTimes()
+
+    whenExecuting(node) {
+      val nodeData = ConfigNodeData(node)
+      assert(NodeName === nodeData.toString)
+    }
+  }
+}
