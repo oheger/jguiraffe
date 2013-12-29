a1d7272365733c8c458fde28d5f31b7c3ef541ef
oheger <oliver.heger@oliver-heger.de>
2013-12-29 12:03:46 +0100

    Added FxThreadModelChangeListener class.
    This class ensures that notifications about tree model changes are processed
    in the JavaFX event dispatch thread.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/tree/FxThreadModelChangeListener.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.tree
+
+import org.apache.commons.configuration.tree.ConfigurationNode
+
+import javafx.application.Platform
+import net.sf.jguiraffe.gui.builder.components.model.TreeModelChangeListener
+
+/**
+ * A specialized implementation of ''TreeModelChangeListener'' that propagates
+ * incoming events to another listener object in the JavaFX thread.
+ *
+ * Events reporting changes of a tree's model can occur in any thread. The
+ * processing of these events leads to manipulations on the UI and thus can
+ * only take place in the JavaFX thread. When setting up a JavaFX tree view
+ * and a corresponding component handler an instance of this class is created
+ * which bridges between the different threads.
+ *
+ * @param listener the wrapped listener
+ */
+private class FxThreadModelChangeListener(val listener: TreeModelChangeListener)
+  extends TreeModelChangeListener {
+  /**
+   * @inheritdoc This implementation propagates the current change event to the
+   * wrapped listener, but in the JavaFX thread.
+   */
+  override def treeModelChanged(node: ConfigurationNode) {
+    Platform.runLater(new Runnable {
+      def run() {
+        listener treeModelChanged node
+      }
+    })
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/tree/TestFxThreadModelChangeListener.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.tree
+
+import java.util.concurrent.CountDownLatch
+
+import org.apache.commons.configuration.tree.ConfigurationNode
+import org.easymock.EasyMock
+import org.easymock.IAnswer
+import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+
+import javafx.application.Platform
+import net.sf.jguiraffe.gui.builder.components.model.TreeModelChangeListener
+import net.sf.jguiraffe.gui.platform.javafx.JavaFxTestHelper
+
+/**
+ * Companion object for ''TestFxThreadModelChangeListener''.
+ */
+object TestFxThreadModelChangeListener {
+  @BeforeClass def setUpOnce() {
+    JavaFxTestHelper.initPlatform()
+  }
+}
+
+/**
+ * Test class for ''FxThreadModelChangeListener''.
+ */
+class TestFxThreadModelChangeListener extends JUnitSuite with EasyMockSugar {
+  /**
+   * Tests whether a change notification is correctly propagated.
+   */
+  @Test def testEventPropagation() {
+    val wrappedListener = mock[TreeModelChangeListener]
+    val node = mock[ConfigurationNode]
+    val latch = new CountDownLatch(1)
+    wrappedListener.treeModelChanged(node)
+    EasyMock.expectLastCall().andAnswer(new IAnswer[Object] {
+      def answer(): Object = {
+        assertTrue("Not in FX thread", Platform.isFxApplicationThread)
+        latch.countDown()
+        null
+      }
+    })
+
+    whenExecuting(wrappedListener) {
+      val listener = new FxThreadModelChangeListener(wrappedListener)
+      listener treeModelChanged node
+      JavaFxTestHelper.await(latch)
+    }
+  }
+}
