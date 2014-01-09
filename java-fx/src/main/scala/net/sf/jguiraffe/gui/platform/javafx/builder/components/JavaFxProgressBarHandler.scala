33f3387a2956c488873b6113fe91f38dd3b44a35
oheger <oliver.heger@oliver-heger.de>
2014-01-09 17:24:22 +0100

    Added a ComponentHandler implementation for JavaFX ProgressBar components.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/JavaFxProgressBarHandler.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import scala.beans.BeanProperty
+
+import javafx.scene.control.ProgressBar
+import net.sf.jguiraffe.gui.builder.components.model.ProgressBarHandler
+import net.sf.jguiraffe.gui.platform.javafx.builder.event.ChangeEventSource
+
+/**
+ * The JavaFX ''ComponentHandler'' implementation for progress bar components.
+ *
+ * This handler can be used to display a progress bar. The current value of the
+ * bar can be set as an integer value - the borders for this value are passed
+ * to the handler at construction time; it is scaled to the double values
+ * between 0 and 1 as accepted by the JavaFX ''ProgressBar'' component.
+ *
+ * Change events are supported; they are generated when the progress bar's
+ * value is changed.
+ *
+ * JavaFX does not support setting a string value for the current progress.
+ * Therefore, the ''progressText'' property required by the
+ * ''ProgressBarHandler'' interface is implemented as a dummy.
+ *
+ * @param progressBar the underlying ''ProgressBar'' component
+ * @param min the minimum value of the progress bar
+ * @param max the maximum value of the progress bar
+ */
+private class JavaFxProgressBarHandler(progressBar: ProgressBar,
+  val min: Int, val max: Int) extends JavaFxComponentHandler[Integer](progressBar)
+  with ProgressBarHandler with ChangeEventSource {
+  /** The data type of this handler. */
+  @BeanProperty val `type` = classOf[Integer]
+
+  /** The property used for generating change events. */
+  override val observableValue = progressBar.progressProperty
+
+  /**
+   * Stores the progress text. JavaFX does not support an additional text
+   * property. Therefore, the value passed to this property is only stored in
+   * an internal member field.
+   */
+  @BeanProperty var progressText: String = _
+
+  /**
+   * @inheritdoc This implementation just delegates to ''getValue''.
+   */
+  def getData: Integer = getValue
+
+  /**
+   * @inheritdoc This implementation delegates to ''setValue()''. If the
+   * passed in value is '''null''', this call has no effect.
+   */
+  def setData(value: Integer) {
+    if (value != null) {
+      setValue(value.intValue)
+    }
+  }
+
+  /**
+   * @inheritdoc This implementation obtains the current progress value from
+   * the ''ProgressBar'' and maps it to the current range as an integer.
+   */
+  def getValue: Int = min + (progressBar.getProgress * (max - min)).toInt
+
+  /**
+   * @inheritdoc This implementation expects that the passed in value is in
+   * the range determined by the ''min'' and ''max'' parameters. It maps the
+   * value to a percentage value in the interval [0, 1].
+   */
+  def setValue(v: Int) {
+    val progress = (v - min).toDouble / (max - min).toDouble
+    progressBar setProgress progress
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/TestJavaFxProgressBarHandler.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2006-2013 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertSame
+import org.junit.Before
+import org.junit.Test
+
+import javafx.scene.control.ProgressBar
+
+/**
+ * Test class for ''JavaFxProgressBarHandler''.
+ */
+class TestJavaFxProgressBarHandler {
+  /** The progress bar control. */
+  private var progressBar: ProgressBar = _
+
+  /** The handler to be tested. */
+  private var handler: JavaFxProgressBarHandler = _
+
+  @Before def setUp() {
+    progressBar = new ProgressBar
+    handler = new JavaFxProgressBarHandler(progressBar, 0, 100)
+  }
+
+  /**
+   * Tests whether the correct data type is returned for the handler.
+   */
+  @Test def testType() {
+    assertEquals("Wrong type", classOf[Integer], handler.getType)
+  }
+
+  /**
+   * Tests whether the correct property for change notifications is returned.
+   */
+  @Test def testChangeEventSupport() {
+    assertSame("Wrong change event property", progressBar.progressProperty,
+      handler.observableValue)
+  }
+
+  /**
+   * Tests whether the correct current progress value can be queried.
+   */
+  @Test def testGetValue() {
+    def checkValue(p: Double, expVal: Int) {
+      progressBar setProgress p
+      assertEquals("Wrong value", expVal, handler.getValue)
+    }
+
+    checkValue(0, 0)
+    checkValue(0.25, 25)
+    checkValue(0.5, 50)
+    checkValue(0.75, 75)
+    checkValue(1, 100)
+  }
+
+  /**
+   * Tests whether the progress bar's value can be set directly.
+   */
+  @Test def testSetValue() {
+    def checkSetValue(value: Int, expProgr: Double) {
+      handler setValue value
+      assertEquals("Wrong progress", expProgr, progressBar.getProgress, .001)
+    }
+
+    checkSetValue(0, 0)
+    checkSetValue(25, .25)
+    checkSetValue(50, .5)
+    checkSetValue(75, .75)
+    checkSetValue(100, 1)
+  }
+
+  /**
+   * Tests whether the handler's data can be queried.
+   */
+  @Test def testGetData() {
+    progressBar setProgress .5
+    assertEquals("Wrong value", Integer.valueOf(50), handler.getData)
+  }
+
+  /**
+   * Tests whether the handler's data can be set to a valid value.
+   */
+  @Test def testSetData() {
+    handler setData Integer.valueOf(50)
+    assertEquals("Wrong progress value", .5, progressBar.getProgress, .001)
+  }
+
+  /**
+   * Tests whether setData() can deal with null values.
+   */
+  @Test def testSetDataNull() {
+    val progress = .2
+    progressBar setProgress progress
+    handler setData null
+    assertEquals("Progress was changed", progress, progressBar.getProgress, .001)
+  }
+
+  /**
+   * Tests the handling of the progressText property. This property is not
+   * supported by JavaFX, so the value passed to the property is just recorded.
+   */
+  @Test def testProgressText() {
+    val text = "Test progress text"
+    handler setProgressText text
+    assertEquals("Wrong progress text", text, handler.getProgressText)
+  }
+}
