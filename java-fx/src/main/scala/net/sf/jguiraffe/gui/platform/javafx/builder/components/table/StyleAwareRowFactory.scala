26e68197dedeaf3f8d0fc9a5555874850cd2cc6b
oheger <oliver.heger@oliver-heger.de>
2014-08-18 22:35:11 +0200

    Added StyleAwareRowFactory class.
    This is a specialized row factory for a table view. It manipulates the
    styles definition of the rows it created dynamically based on their
    selection flag.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/table/StyleAwareRowFactory.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.table
+
+import javafx.beans.binding.Bindings
+import javafx.beans.property.{SimpleStringProperty, StringProperty}
+import javafx.scene.control.{TableRow, TableView}
+import javafx.util.Callback
+
+import org.apache.commons.lang.StringUtils
+
+/**
+ * This is a specialized row factory for table views that handles specific styles to be
+ * applied on selected rows.
+ *
+ * ''JGUIraffe'' supports setting the foreground and background colors for selected
+ * rows in a table view explicitly. This is surprisingly difficult to realize with
+ * JavaFX because the default way here is to use (static) CSS. In order to apply
+ * colors programmatically, a component's ''style'' property has to be changed
+ * dynamically. This implementation does exactly this for a table row which is in fact
+ * a ''Cell'' instance.
+ *
+ * What this class does is to create a binding between a table row's ''selected''
+ * property and a string property allowing the definition of specific styles. If
+ * the row is selected, the style is set to the content of this text property;
+ * otherwise, it is set to an empty string. The text property for the styles
+ * definition is passed to the
+ * [[net.sf.jguiraffe.gui.platform.javafx.builder.components.table.JavaFxTableHandler]]
+ * object. Here corresponding styles are added when selection colors are modified.
+ */
+private class StyleAwareRowFactory[S] extends Callback[TableView[S], TableRow[S]] {
+  this: CellSelectionExtractor =>
+  /** The property for setting the style definition. */
+  val styleProperty: StringProperty = new SimpleStringProperty(StringUtils.EMPTY)
+
+  /** The property containing an empty style definition. */
+  private val emptyStyle = new SimpleStringProperty(StringUtils.EMPTY)
+
+  override def call(tableView: TableView[S]): TableRow[S] =
+    bindStyleProperty(new TableRow[S])
+
+  /**
+   * Binds the style property of the given row to a string property based on the
+   * current value of its selected property.
+   * @param row the row to be manipulated
+   * @return the same row
+   */
+  private def bindStyleProperty(row: TableRow[S]): TableRow[S] = {
+    row.styleProperty() bind Bindings.when(selectedProperty(row))
+      .then(styleProperty)
+      .otherwise(emptyStyle)
+    row
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/table/TestStyleAwareRowFactory.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components.table
+
+import javafx.beans.property.{BooleanProperty, ReadOnlyBooleanProperty, SimpleBooleanProperty}
+import javafx.scene.control.{Cell, TableRow, TableView}
+
+import org.junit.Assert._
+import org.junit.{Before, Test}
+import org.scalatest.junit.JUnitSuite
+
+/**
+ * Test class for ''StyleAwareRowFactory''.
+ */
+class TestStyleAwareRowFactory extends JUnitSuite {
+  /** A constant for a test style definition. */
+  private val Style = "-fx-background-color: #CCDDEE;"
+
+  /** The test property for manipulating the selection. */
+  private var testSelectedProperty: BooleanProperty = _
+
+  /** The factory to be tested. */
+  private var factory: StyleAwareRowFactory[AnyRef] = _
+
+  @Before def setUp(): Unit = {
+    testSelectedProperty = new SimpleBooleanProperty(false)
+    factory = new StyleAwareRowFactory[AnyRef] with MockSelectionExtractor
+  }
+
+  /**
+   * Creates a row using the test factory.
+   * @return the table row
+   */
+  private def createRow(): TableRow[AnyRef] = {
+    factory.styleProperty set Style
+    val table = new TableView[AnyRef]
+    factory call table
+  }
+
+  /**
+   * Tests the style if the row is not selected.
+   */
+  @Test def testStyleNotSelected(): Unit = {
+    val row = createRow()
+    assertFalse("Selected", row.isSelected)
+    assertEquals("Got styles", "", row.getStyle)
+  }
+
+  /**
+   * Tests whether the style is changed if the row is selected.
+   */
+  @Test def testStyleSelected(): Unit = {
+    val row = createRow()
+    testSelectedProperty set true
+    assertEquals("Wrong style", Style, row.getStyle)
+  }
+
+  /**
+   * A mock implementation of ''CellSelectionExtractor'' allowing to use a
+   * test property.
+   */
+  private trait MockSelectionExtractor extends CellSelectionExtractor {
+    override def selectedProperty(cell: Cell[_]): ReadOnlyBooleanProperty = testSelectedProperty
+  }
+
+}
