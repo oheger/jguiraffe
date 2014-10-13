d5cc0adfdb332b8fdf7999846c8191628e460254
oheger <oliver.heger@oliver-heger.de>
2014-10-13 21:59:01 +0200

    Added MenuItemComponentHandler class.
    This is an internally used ComponentHandler implementation for menu items. Such
    a component handler is required by the ActionManager interface.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/MenuItemComponentHandler.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.action
+
+import javafx.beans.property.BooleanProperty
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.scene.control.MenuItem
+
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+import net.sf.jguiraffe.gui.platform.javafx.builder.event.ActionEventSource
+
+/**
+ * An internally used ''ComponentHandler'' implementation which wraps a JavaFX
+ * ''MenuItem''.
+ *
+ * Instances of this class are returned by the action manager's
+ * ''createMenuItem()'' method which is passed an action data object. Rather
+ * than returning the newly created menu item as an untyped object, the
+ * ''ActionManager'' interface requires the method to return a
+ * ''ComponentHandler''. This allows client code to manipulate the menu item
+ * via the ''ComponentHandler'' interface and also to register event listeners.
+ *
+ * Unfortunately, it is not possible to reuse the ''ComponentHandler''
+ * implementation for JavaFX controls because ''MenuItem'' does not extend the
+ * ''Node'' base class. Therefore, a full implementation of the handler
+ * interface is required.
+ *
+ * This handler implementation deals with normal menu items and checked items.
+ * Its data is the selected state of the menu item. This data is simply mapped
+ * to a boolean property passed to the constructor. The creating instance is
+ * responsible for setting the correct property.
+ *
+ * @param item the wrapped menu item
+ * @param property the property storing the data of this handler
+ * @param actionCommand the command to be published for action events
+ */
+private class MenuItemComponentHandler(val item: MenuItem, val property: BooleanProperty,
+                                       override val actionCommand: String)
+  extends ComponentHandler[java.lang.Boolean] with ActionEventSource {
+  override val getComponent = item
+
+  override val getOuterComponent = item
+
+  val getType = classOf[java.lang.Boolean]
+
+  /**
+   * @inheritdoc
+   * This implementation returns the value of the associated boolean property.
+   */
+  override def getData: java.lang.Boolean = property.get
+
+  /**
+   * @inheritdoc
+   * This implementation writes the passed in data into the associated property.
+   */
+  override def setData(data: java.lang.Boolean): Unit = {
+    property setValue data
+  }
+
+  /**
+   * @inheritdoc
+   * This implementation returns the negation of the menu item's disabled
+   * property.
+   */
+  override def isEnabled: Boolean = !item.isDisable
+
+  /**
+   * @inheritdoc
+   * This implementation sets the menu item's disabled state to the negation of
+   * the passed in flag.
+   */
+  override def setEnabled(f: Boolean): Unit = item setDisable !f
+
+  /**
+   * @inheritdoc
+   * This implementation adds the specified handler to the wrapped menu item.
+   */
+  override def addActionListener(handler: EventHandler[ActionEvent]): Unit =
+    item.addEventHandler(ActionEvent.ACTION, handler)
+
+  /**
+   * @inheritdoc
+   * This implementation removes the specified handler from the wrapped menu
+   * item.
+   */
+  override def removeActionListener(handler: EventHandler[ActionEvent]): Unit =
+    item.removeEventHandler(ActionEvent.ACTION, handler)
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/TestMenuItemComponentHandler.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.action
+
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.scene.control.MenuItem
+
+import org.easymock.EasyMock
+import org.junit.Assert._
+import org.junit.{Before, Test}
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+
+/**
+ * Test class for ''MenuItemComponentHandler''.
+ */
+class TestMenuItemComponentHandler extends JUnitSuite with EasyMockSugar {
+  /** The wrapped menu item. */
+  private var menuItem: MenuItem = _
+
+  /** The property with the handler's data. */
+  private var property: SimpleBooleanProperty = _
+
+  /** The handler to be tested. */
+  private var handler: MenuItemComponentHandler = _
+
+  @Before def setUp(): Unit = {
+    menuItem = new MenuItem
+    property = new SimpleBooleanProperty
+    handler = new MenuItemComponentHandler(menuItem, property, "MyAction")
+  }
+
+  /**
+   * Tests whether the correct component is returned.
+   */
+  @Test def testGetComponent(): Unit = {
+    assertSame("Wrong component", menuItem, handler.getComponent)
+  }
+
+  /**
+   * Tests whether the correct outer component is returned.
+   */
+  @Test def testGetOuterComponent(): Unit = {
+    assertSame("Wrong outer component", menuItem, handler.getOuterComponent)
+  }
+
+  /**
+   * Tests the handler's type.
+   */
+  @Test def testGetType(): Unit = {
+    assertEquals("Wrong type", classOf[java.lang.Boolean], handler.getType)
+  }
+
+  /**
+   * Tests whether the handler's data can be queried.
+   */
+  @Test def testGetData(): Unit = {
+    assertFalse("Wrong data (1)", handler.getData)
+    property set true
+    assertTrue("Wrong data (2)", handler.getData)
+  }
+
+  /**
+   * Tests whether data can be set.
+   */
+  @Test def testSetData(): Unit = {
+    handler setData true
+    assertTrue("Data not set (1)", property.get)
+    handler setData false
+    assertFalse("Data not set (2)", property.get)
+  }
+
+  /**
+   * Tests whether null input can be handled by setData().
+   */
+  @Test def testSetDataNull(): Unit = {
+    handler setData null
+    assertFalse("Wrong value", property.get)
+    assertFalse("Wrong data", handler.getData)
+  }
+
+  /**
+   * Tests whether the enabled state can be queried.
+   */
+  @Test def testIsEnabled(): Unit = {
+    assertTrue("Wrong enabled state (1)", handler.isEnabled)
+    menuItem setDisable true
+    assertFalse("Wrong enabled state (2)", handler.isEnabled)
+  }
+
+  /**
+   * Tests whether the enabled state can be set.
+   */
+  @Test def testSetEnabled(): Unit = {
+    handler setEnabled false
+    assertTrue("Wrong disabled state (1)", menuItem.isDisable)
+    handler setEnabled true
+    assertFalse("Wrong disabled state (2)", menuItem.isDisable)
+  }
+
+  /**
+   * Tests whether an action listener can be added.
+   */
+  @Test def testAddActionListener(): Unit = {
+    val listener = mock[EventHandler[ActionEvent]]
+    listener handle EasyMock.anyObject(classOf[ActionEvent])
+
+    whenExecuting(listener) {
+      handler addActionListener listener
+      menuItem.fire()
+    }
+  }
+
+  /**
+   * Tests whether an action listener can be removed.
+   */
+  @Test def testRemoveActionListener(): Unit = {
+    val listener = mock[EventHandler[ActionEvent]]
+
+    whenExecuting(listener) {
+      handler addActionListener listener
+      handler removeActionListener listener
+      menuItem.fire()
+    }
+  }
+}
