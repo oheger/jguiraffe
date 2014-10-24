74fefa42c5f6a3c76dd85e1d8ae9a802059cab86
oheger <oliver.heger@oliver-heger.de>
2014-10-24 21:50:53 +0200

    Added StyleWidgetHandler.
    This trait implements fundamental functionality for JavaFX widgets related to
    CSS styles.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/StyleWidgetHandler.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import javafx.beans.property.StringProperty
+
+import net.sf.jguiraffe.gui.builder.components.{Color, WidgetHandler}
+
+/**
+ * A trait implementing basic functionality for JavaFX widget handlers related
+ * to CSS styles.
+ *
+ * The problem with widget handlers in JavaFX is that there is no single base
+ * class for widgets; rather, multiple concrete object types - not connected in
+ * an inheritance hierarchy - have to be supported.
+ *
+ * This trait provides common functionality for all kind of widget handlers
+ * based on CSS styles. Concrete subclasses have to provide access to the
+ * actual widget's style property.
+ */
+private trait StyleWidgetHandler extends WidgetHandler {
+  /** The styles handler used by this object. */
+  lazy val stylesHandler = createStylesHandler()
+
+  /** The property defining the style of the wrapped widget. */
+  val style: StringProperty
+
+  /**
+   * @inheritdoc The background color is obtained from the wrapped node's
+   *             styles.
+   */
+  def getBackgroundColor: Color = stylesHandler.getBackgroundColor
+
+  /**
+   * @inheritdoc This implementation passes the color to the styles handler.
+   *             Then all styles are transformed to text and passed to the wrapped
+   *             node.
+   */
+  def setBackgroundColor(c: Color) {
+    stylesHandler setBackgroundColor c
+    updateWidgetStyles()
+  }
+
+  /**
+   * @inheritdoc The foreground color is obtained from the wrapped node's
+   *             styles.
+   */
+  def getForegroundColor: Color = stylesHandler.getForegroundColor()
+
+  /**
+   * @inheritdoc This implementation passes the color to the styles handler.
+   *             Then all styles are transformed to text and passed to the wrapped
+   *             node.
+   */
+  def setForegroundColor(c: Color) {
+    stylesHandler setForegroundColor c
+    updateWidgetStyles()
+  }
+
+  /**
+   * @inheritdoc The node's font is dynamically created based on its styles
+   *             definitions.
+   */
+  def getFont: JavaFxFont = stylesHandler.getFont
+
+  /**
+   * @inheritdoc This implementation expects that the argument is of type
+   *             ''JavaFxFont''. It applies the data of the font object to the wrapped
+   *             node's styles definition,
+   */
+  def setFont(font: Object) {
+    stylesHandler.setFont(font.asInstanceOf[JavaFxFont])
+    updateWidgetStyles()
+  }
+
+  /**
+   * Creates the object for managing the wrapped widget's styles. This
+   * object is created lazily on first access to properties which read or
+   * update style definitions.
+   * @return the style handler to be used by this object
+   */
+  private[components] def createStylesHandler(): JavaFxStylesHandler = {
+    val styles = Styles(style.get)
+    new JavaFxStylesHandler(styles)
+  }
+
+  /**
+   * Updates the styles of the wrapped widget. This method is called whenever
+   * a property was changed that is implemented through CSS styles.
+   */
+  private def updateWidgetStyles() {
+    style set stylesHandler.styles.toExternalForm()
+  }
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/TestStyleWidgetHandler.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.components
+
+import javafx.beans.property.{SimpleStringProperty, StringProperty}
+
+import net.sf.jguiraffe.gui.builder.components.Color
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Assert._
+import org.junit.{Before, Test}
+
+/**
+ * Test class for ''StyleWidgetHandler''.
+ */
+class TestStyleWidgetHandler {
+  /** The style property used by the test handler. */
+  private var styleProperty: StringProperty = _
+
+  /** The handler to be tested. */
+  private var handler: StyleWidgetHandler = _
+
+  @Before def setUp(): Unit = {
+    styleProperty = new SimpleStringProperty("-fx-unknown: 1;")
+    handler = new StyleWidgetHandler {
+      override val style: StringProperty = styleProperty
+
+      override def isVisible: Boolean = unexpected()
+
+      override def setVisible(f: Boolean): Unit = unexpected()
+
+      override def getToolTip: String = unexpected()
+
+      override def getWidget: AnyRef = unexpected()
+
+      override def setToolTip(tip: String): Unit = unexpected()
+
+      private def unexpected(): Nothing = throw new AssertionError("Unexpected method call!")
+    }
+  }
+
+  /**
+   * Generates a style definition.
+   * @param key the key
+   * @param value the value
+   * @return the complete style definition
+   */
+  private def generateStyle(key: String, value: String): String = s"$key: $value;"
+
+  /**
+   * Checks whether the style property contains the expected style definition.
+   * @param key the key of the expected definition
+   * @param value the value of the expected definition
+   */
+  private def checkStyle(key: String, value: String): Unit = {
+    assertThat(styleProperty.get, containsString(generateStyle(key, value)))
+  }
+
+  /**
+   * Writes an entry into the style property for the specified key.
+   * @param key the key
+   * @param value the value
+   */
+  private def initStyleProperty(key: String, value: String): Unit = {
+    styleProperty setValue generateStyle(key, value)
+  }
+
+  /**
+   * Tests whether the foreground color is correctly extracted.
+   */
+  @Test def testGetForegroundColor(): Unit = {
+    val ColorValue = "fgcol"
+    initStyleProperty(JavaFxStylesHandler.StyleForegroundColor, ColorValue)
+    val color = handler.getForegroundColor
+    assertEquals("Wrong color", ColorValue, color.getColorDefinition)
+  }
+
+  /**
+   * Tests whether the foreground color can be set.
+   */
+  @Test def testSetForegroundColor(): Unit = {
+    val ColorValue = "fgcol"
+    handler setForegroundColor Color.newLogicInstance(ColorValue)
+    checkStyle(JavaFxStylesHandler.StyleForegroundColor, ColorValue)
+  }
+
+  /**
+   * Tests whether the background color is correctly extracted.
+   */
+  @Test def testGetBackgroundColor(): Unit = {
+    val ColorValue = "bgcol"
+    initStyleProperty(JavaFxStylesHandler.StyleBackgroundColor, ColorValue)
+    val color = handler.getBackgroundColor
+    assertEquals("Wrong color", ColorValue, color.getColorDefinition)
+  }
+
+  /**
+   * Tests whether the background color can be set.
+   */
+  @Test def testSetBackgroundColor(): Unit = {
+    val ColorValue = "backGround"
+    handler setBackgroundColor Color.newLogicInstance(ColorValue)
+    checkStyle(JavaFxStylesHandler.StyleBackgroundColor, ColorValue)
+  }
+
+  /**
+   * Tests whether the font can be queried.
+   */
+  @Test def testGetFont(): Unit = {
+    val FontFamily = "MyTestFont"
+    initStyleProperty(JavaFxStylesHandler.StyleFontFamily, FontFamily)
+    val font = handler.getFont
+    assertEquals("Wrong font", FontFamily, font.family.get)
+  }
+
+  /**
+   * Tests whether the font can be set.
+   */
+  @Test def testSetFont(): Unit = {
+    val font = JavaFxFont(family = Some("TestFont"))
+    handler setFont font
+    checkStyle(JavaFxStylesHandler.StyleFontFamily, font.family.get)
+  }
+}
