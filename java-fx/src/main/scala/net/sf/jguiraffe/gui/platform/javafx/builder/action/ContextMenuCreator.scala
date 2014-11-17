dfc33ea909f12b255738320fee53d619bcb26e7c
oheger <oliver.heger@oliver-heger.de>
2014-11-17 22:17:51 +0100

    Added ContextMenuCreator trait.
    This trait will be used internally to obtain a new ContextMenu instance.
    Added a common test class for functionality related to popup menus.

---
+++ java-fx/src/main/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/ContextMenuCreator.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.action
+
+import javafx.scene.control.ContextMenu
+
+/**
+ * A trait for creating a JavaFX context menu.
+ *
+ * This trait is used internally for obtaining a new context menu instance.
+ * Per default, simply a ''ContextMenu'' object is created and returned.
+ * Test classes can inject a mock implementation.
+ */
+private trait ContextMenuCreator {
+  /**
+   * Creates a new ''ContextMenu'' instance. This implementation just creates
+   * a new instance without any special initialization.
+   * @return the newly created context menu instance
+   */
+  def createContextMenu(): ContextMenu = new ContextMenu
+}
---
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/TestPopupMenuHandling.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2006-2014 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.javafx.builder.action
+
+import javafx.event.EventType
+import javafx.scene.input.{MouseButton, MouseEvent}
+
+import net.sf.jguiraffe.gui.platform.javafx.JavaFxTestHelper
+import org.easymock.EasyMock
+import org.junit.Assert._
+import org.junit.runner.RunWith
+import org.junit.{BeforeClass, Test}
+import org.powermock.api.easymock.PowerMock
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import org.scalatest.junit.JUnitSuite
+
+/**
+ * Companion object.
+ */
+object TestPopupMenuHandling {
+  /** A default x coordinate for a mouse event. */
+  private val EventX = 128
+
+  /** A default y coordinate for a mouse event. */
+  private val EventY = 222
+
+  @BeforeClass def setUpOnce(): Unit = {
+    JavaFxTestHelper.initPlatform()
+  }
+
+  /**
+   * Creates a mock four a mouse event with some default properties.
+   * @param eventType the event type
+   * @param btn the mouse button that was pressed
+   * @return the mock for the event
+   */
+  private def createEvent(eventType: EventType[MouseEvent],
+                          btn: MouseButton = MouseButton.PRIMARY): MouseEvent = {
+    val event = PowerMock.createNiceMock(classOf[MouseEvent])
+    event.getEventType
+    EasyMock.expectLastCall().andReturn(eventType).anyTimes()
+    EasyMock.expect(event.getButton).andReturn(btn).anyTimes()
+    EasyMock.expect(event.getX).andReturn(EventX).anyTimes()
+    EasyMock.expect(event.getY).andReturn(EventY).anyTimes()
+    event
+  }
+}
+
+/**
+ * A test class for the functionality related to popup menu support.
+ *
+ * This functionality is implemented by a couple of internal classes. Testing
+ * requires support by PowerMock to gain access to final methods in the JavaFX
+ * API. Therefore, these tests were extracted into their own test class.
+ */
+@RunWith(classOf[PowerMockRunner])
+@PrepareForTest(Array(classOf[MouseEvent]))
+class TestPopupMenuHandling extends JUnitSuite {
+  /**
+   * Tests whether a correct context menu is created.
+   */
+  @Test def testContextMenuCreator(): Unit = {
+    val creator = new ContextMenuCreator {}
+    val menu = JavaFxTestHelper.invokeInFxThread { f => creator.createContextMenu()}
+    assertTrue("Got items", menu.getItems.isEmpty)
+  }
+}
