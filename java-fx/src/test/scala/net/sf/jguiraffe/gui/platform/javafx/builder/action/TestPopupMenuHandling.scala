9a4203ba143e508d23a1de554d55f2f76396f6fb
oheger <oliver.heger@oliver-heger.de>
2014-11-19 21:49:20 +0100

    Implemented JavaFxPopupMenuBuilder class.
    This is an implementation of the PopopMenuBuilder interface for JavaFX context
    menus.

--- java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/TestPopupMenuHandling.scala
+++ java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/action/TestPopupMenuHandling.scala
@@ -16,8 +16,10 @@
 package net.sf.jguiraffe.gui.platform.javafx.builder.action
 
 import javafx.event.EventType
+import javafx.scene.control.{ContextMenu, Label}
 import javafx.scene.input.{MouseButton, MouseEvent}
 
+import net.sf.jguiraffe.gui.builder.action.{ActionBuilder, ActionManager}
 import net.sf.jguiraffe.gui.platform.javafx.JavaFxTestHelper
 import org.easymock.EasyMock
 import org.junit.Assert._
@@ -48,14 +50,14 @@ object TestPopupMenuHandling {
    * @param btn the mouse button that was pressed
    * @return the mock for the event
    */
-  private def createEvent(eventType: EventType[MouseEvent],
+  private def createEvent(eventType: EventType[MouseEvent] = MouseEvent.MOUSE_CLICKED,
                           btn: MouseButton = MouseButton.PRIMARY): MouseEvent = {
     val event = PowerMock.createNiceMock(classOf[MouseEvent])
     event.getEventType
     EasyMock.expectLastCall().andReturn(eventType).anyTimes()
     EasyMock.expect(event.getButton).andReturn(btn).anyTimes()
-    EasyMock.expect(event.getX).andReturn(EventX).anyTimes()
-    EasyMock.expect(event.getY).andReturn(EventY).anyTimes()
+    EasyMock.expect(event.getScreenX).andReturn(EventX).anyTimes()
+    EasyMock.expect(event.getScreenY).andReturn(EventY).anyTimes()
     event
   }
 }
@@ -70,6 +72,9 @@ object TestPopupMenuHandling {
 @RunWith(classOf[PowerMockRunner])
 @PrepareForTest(Array(classOf[MouseEvent]))
 class TestPopupMenuHandling extends JUnitSuite {
+
+  import net.sf.jguiraffe.gui.platform.javafx.builder.action.TestPopupMenuHandling._
+
   /**
    * Tests whether a correct context menu is created.
    */
@@ -77,5 +82,71 @@ class TestPopupMenuHandling extends JUnitSuite {
     val creator = new ContextMenuCreator {}
     val menu = JavaFxTestHelper.invokeInFxThread { f => creator.createContextMenu()}
     assertTrue("Got items", menu.getItems.isEmpty)
+  }
+
+  /**
+   * Tests whether a menu builder is correctly initialized.
+   */
+  @Test def testMenuBuilderInitialization(): Unit = {
+    val actionManager = PowerMock.createMock(classOf[ActionManager])
+    val actionBuilder = PowerMock.createMock(classOf[ActionBuilder])
+    val menuBuilder = new JavaFxPopupMenuBuilder(actionManager, actionBuilder, new Label,
+      createEvent()) with MockContextMenuCreator
+
+    assertSame("Wrong action manager", actionManager, menuBuilder.getActionManager)
+    assertSame("Wrong action builder", actionBuilder, menuBuilder.getActionBuilder)
+  }
+
+  /**
+   * Tests whether the menu builder returns the correct menu under construction.
+   */
+  @Test def testMenuBuilderMenuUnderConstruction(): Unit = {
+    val actionManager = PowerMock.createMock(classOf[ActionManager])
+    val actionBuilder = PowerMock.createMock(classOf[ActionBuilder])
+    val menuBuilder = new JavaFxPopupMenuBuilder(actionManager, actionBuilder, new Label,
+      createEvent()) with MockContextMenuCreator
+    PowerMock.replayAll()
+
+    assertSame("Wrong menu under construction", menuBuilder.mockMenu, menuBuilder
+      .getMenuUnderConstruction)
+  }
+
+  /**
+   * Tests the create() method of the popup menu builder.
+   */
+  @Test def testMenuBuilderCreate(): Unit = {
+    val node = new Label
+    val menuBuilder = new JavaFxPopupMenuBuilder(PowerMock.createMock(classOf[ActionManager]),
+      PowerMock.createMock(classOf[ActionBuilder]), node, createEvent()) with MockContextMenuCreator
+    menuBuilder.mockMenu.show(node, EventX, EventY)
+    PowerMock.replayAll()
+
+    assertSame("Wrong menu", menuBuilder.mockMenu, menuBuilder.create())
+    PowerMock.verify(menuBuilder.mockMenu)
+  }
+}
+
+/**
+ * A mock implementation of a context menu creator.
+ *
+ * This implementation has a mock context menu which is returned on each call.
+ * It also checks that it is called at most once.
+ */
+private trait MockContextMenuCreator extends ContextMenuCreator {
+  /** The mock context menu. */
+  val mockMenu = PowerMock.createMock(classOf[ContextMenu])
+
+  /** A counter for the invocations. */
+  var createContextMenuCount = 0
+
+  /**
+   * Creates a new ''ContextMenu'' instance. This implementation just creates
+   * a new instance without any special initialization.
+   * @return the newly created context menu instance
+   */
+  override def createContextMenu(): ContextMenu = {
+    assertTrue("Too many invocations", createContextMenuCount < 1)
+    createContextMenuCount += 1
+    mockMenu
   }
 }
