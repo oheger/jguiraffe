2c9b565d79e2230103f20aa60e5a1fe018a0f8c9
oheger <oliver.heger@oliver-heger.de>
2018-12-01 18:12:18 +0100

    [44]: Added new RadioGroupWidgetHandler class to core.
    This class implements the WidgetHandler interface for radio groups in
    a generic fashion. The code is based on the Swing-specific
    implementation, but was generalized. It can now be used by all UI
    libraries to implement this specific widget type.

---
+++ parent/core/src/main/java/net/sf/jguiraffe/gui/builder/components/RadioGroupWidgetHandler.java
@@ -0,0 +1,280 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.builder.components;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.ObjectUtils;
+
+/**
+ * <p>
+ * A specialized {@link WidgetHandler} implementation for radio groups.
+ * </p>
+ * <p>
+ * Radio groups in UI libraries are typically somewhat special. They are not
+ * graphical components on their own, but merely refer to the actual radio
+ * button components. However, in the <em>JGUIraffe</em> library radio groups
+ * are more or less treated like regular components, and their properties can be
+ * accessed through the {@link WidgetHandler} interface.
+ * </p>
+ * <p>
+ * Because of this a special implementation is needed for radio buttons. This
+ * implementations acts like a typical composite. Set operations are delegated
+ * to all components in the group. For get operations there are some
+ * limitations: Unless noted otherwise for a specific operation, the group
+ * widget handler expects that all radio buttons in the group share the same
+ * properties (e.g. colors or visible state). Therefore it simply returns the
+ * corresponding property of the first button in the group.
+ * </p>
+ * <p>
+ * Implementation notes: This implementation is fully functional and can be used
+ * independent on the underlying UI library. This is possible because radio
+ * button controls are accessed only via the {@code WidgetHandler} interface.
+ * This class is not thread-safe. It should be accessed from the <em>event
+ * dispatch thread</em> only.
+ * </p>
+ *
+ * @since 1.4
+ */
+public class RadioGroupWidgetHandler implements WidgetHandler
+{
+    /** Stores the button group widget. */
+    private final Object buttonGroup;
+
+    /** List with the handlers of the managed radio buttons. */
+    private final List<WidgetHandler> radioButtons;
+
+    /** The separator between two combined tool tips. */
+    private final String tipSeparator;
+
+    /** An array with the original tool tips of the radio buttons. */
+    private final String[] buttonTips;
+
+    /** The tool tip of the whole radio group. */
+    private String groupTip;
+
+    /**
+     * Creates a new instance of {@code RadioGroupWidgetHandler} that handles
+     * the given collection of radio buttons.
+     *
+     * @param group the object representing the button group
+     * @param radios the collection with radio buttons to be managed
+     * @param toolTipSeparator the separator for tool tips
+     */
+    public RadioGroupWidgetHandler(Object group,
+            Collection<? extends WidgetHandler> radios, String toolTipSeparator)
+    {
+        buttonGroup = group;
+        radioButtons = Collections
+                .unmodifiableList(new ArrayList<WidgetHandler>(radios));
+        tipSeparator = toolTipSeparator;
+        buttonTips = new String[radios.size()];
+    }
+
+    /**
+     * Returns a (unmodifiable) list with the widget handlers of the radio
+     * buttons managed by this object.
+     *
+     * @return the handlers of the managed radio buttons
+     */
+    public List<WidgetHandler> getRadioButtons()
+    {
+        return radioButtons;
+    }
+
+    /**
+     * Returns the separator between two combined tool tips.
+     *
+     * @return the tool tip separator
+     */
+    public String getTipSeparator()
+    {
+        return tipSeparator;
+    }
+
+    public Object getWidget()
+    {
+        return buttonGroup;
+    }
+
+    /**
+     * {@inheritDoc} This implementation expects that all buttons in the group
+     * have the same visible state. So only the first button is checked.
+     */
+    public boolean isVisible()
+    {
+        return firstChild().isVisible();
+    }
+
+    /**
+     * {@inheritDoc} This implementation changes the visibility state of all
+     * radio buttons in this group.
+     */
+    public void setVisible(boolean f)
+    {
+        for (WidgetHandler h : getRadioButtons())
+        {
+            h.setVisible(f);
+        }
+    }
+
+    /**
+     * {@inheritDoc} This implementation assumes that all buttons in the group
+     * have the same background color. So the property of the first button is
+     * returned.
+     */
+    public Color getBackgroundColor()
+    {
+        return firstChild().getBackgroundColor();
+    }
+
+    /**
+     * {@inheritDoc} This implementation sets the new background color for all
+     * radio buttons in this group.
+     */
+    public void setBackgroundColor(Color c)
+    {
+        for (WidgetHandler h : getRadioButtons())
+        {
+            h.setBackgroundColor(c);
+        }
+    }
+
+    /**
+     * {@inheritDoc} This implementation assumes that all buttons in the group
+     * have the same foreground color. So the property of the first button is
+     * returned.
+     */
+    public Color getForegroundColor()
+    {
+        return firstChild().getForegroundColor();
+    }
+
+    /**
+     * {@inheritDoc} This implementation sets the new foreground color for all
+     * radio buttons in this group.
+     */
+    public void setForegroundColor(Color c)
+    {
+        for (WidgetHandler h : getRadioButtons())
+        {
+            h.setForegroundColor(c);
+        }
+    }
+
+    /**
+     * {@inheritDoc} This implementation returns the tool tip of the whole radio
+     * group. This tool tip is independent of the tips of single radio buttons.
+     * So this implementation just returns the string that was set using
+     * {@link #setToolTip(String)}.
+     */
+    public String getToolTip()
+    {
+        return groupTip;
+    }
+
+    /**
+     * {@inheritDoc} This implementation sets the overall tool tip, and it
+     * changes the tool tips of the radio buttons that belong to this group. The
+     * tool tips of the radio buttons are generated by their original tool tip,
+     * a separator, plus the group's tool tip.
+     */
+    public void setToolTip(String tip)
+    {
+        int i = 0;
+        for (WidgetHandler h : getRadioButtons())
+        {
+            String expectedTip =
+                    combineToolTips(buttonTips[i], groupTip, getTipSeparator());
+            String currentTip = h.getToolTip();
+            if (!ObjectUtils.equals(expectedTip, currentTip))
+            {
+                // the tip of the button has changed in the mean time
+                buttonTips[i] = currentTip;
+            }
+
+            h.setToolTip(
+                    combineToolTips(buttonTips[i], tip, getTipSeparator()));
+            i++;
+        }
+
+        groupTip = tip;
+    }
+
+    /**
+     * {@inheritDoc} This implementation assumes that all buttons in the group
+     * have the same font. So the property of the first button is returned.
+     */
+    public Object getFont()
+    {
+        return firstChild().getFont();
+    }
+
+    /**
+     * {@inheritDoc} This implementation sets the new font for all radio buttons
+     * in this group.
+     */
+    public void setFont(Object font)
+    {
+        for (WidgetHandler h : getRadioButtons())
+        {
+            h.setFont(font);
+        }
+    }
+
+    /**
+     * Combines the given tool tips (which both can be <strong>null</strong> to
+     * a single one. This method is useful when an already existing tool tip is
+     * to be extended by some additional information.
+     *
+     * @param tip1 the first tool tip
+     * @param tip2 the second tool tip
+     * @param separator the separator between two defined tool tips
+     * @return the resulting combined tool tip; can be <strong>null</strong> if
+     *         both tips are <strong>null</strong>
+     */
+    static String combineToolTips(String tip1, String tip2, String separator)
+    {
+        if (tip1 == null && tip2 == null)
+        {
+            return null;
+        }
+        else if (tip1 == null)
+        {
+            return tip2;
+        }
+        else if (tip2 == null)
+        {
+            return tip1;
+        }
+        return tip1 + separator + tip2;
+    }
+
+    /**
+     * Returns the widget handler for the first child of the button group. This
+     * element plays a special role because it is accessed by all the methods
+     * that query a state property of the group.
+     *
+     * @return the handler for the first child element
+     */
+    private WidgetHandler firstChild()
+    {
+        return getRadioButtons().get(0);
+    }
+}
---
+++ parent/core/src/test/java/net/sf/jguiraffe/gui/builder/components/TestRadioGroupWidgetHandler.java
@@ -0,0 +1,452 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.builder.components;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+/**
+ * Test class for {@code RadioGroupWidgetHandler}.
+ */
+public class TestRadioGroupWidgetHandler
+{
+    /** Some object representing a button group. */
+    private static final Object BUTTON_GROUP = new Object();
+
+    /** Constant for a test tool tip separator. */
+    private static final String TIP_SEPARATOR = "----";
+
+    /**
+     * Returns a list with mock widget handlers simulating radio buttons.
+     *
+     * @return the list with mock widget handlers
+     */
+    private static List<WidgetHandler> createMockRadioHandlers()
+    {
+        final int count = 3;
+        List<WidgetHandler> handlers = new ArrayList<WidgetHandler>(count);
+        for (int i = 0; i < count; i++)
+        {
+            handlers.add(EasyMock.createMock(WidgetHandler.class));
+        }
+        return handlers;
+    }
+
+    /**
+     * Creates a test instance of the widget handler with default properties.
+     *
+     * @return the new test instance
+     */
+    private static RadioGroupWidgetHandler createGroupHandler()
+    {
+        return new RadioGroupWidgetHandler(BUTTON_GROUP,
+                createMockRadioHandlers(), TIP_SEPARATOR);
+    }
+
+    /**
+     * Convenience function to convert the collection of radio button handlers
+     * to an array.
+     *
+     * @param handlers the collection of handlers
+     * @return the corresponding array with handlers
+     */
+    private static WidgetHandler[] radioArray(List<WidgetHandler> handlers)
+    {
+        return handlers.toArray(new WidgetHandler[0]);
+    }
+
+    /**
+     * Convenience function to return the first radio button handler in the
+     * group managed by the given handler.
+     *
+     * @param groupHandler the handler
+     * @return the handler of the first radio button
+     */
+    private static WidgetHandler firstRadio(
+            RadioGroupWidgetHandler groupHandler)
+    {
+        return groupHandler.getRadioButtons().get(0);
+    }
+
+    /**
+     * Replays all mock handlers in the given collection.
+     *
+     * @param handlers the collection with mock handlers
+     */
+    private static void replayRadioHandlers(List<WidgetHandler> handlers)
+    {
+        EasyMock.replay((Object[]) radioArray(handlers));
+    }
+
+    /**
+     * Replays the mocks for the child radio buttons of the given handler.
+     *
+     * @param groupHandler the handler
+     */
+    private static void replayGroup(RadioGroupWidgetHandler groupHandler)
+    {
+        replayRadioHandlers(groupHandler.getRadioButtons());
+    }
+
+    /**
+     * Verifies all mock handlers in the given collection.
+     *
+     * @param handlers the collection of handlers
+     */
+    private static void verifyRadioHandlers(List<WidgetHandler> handlers)
+    {
+        EasyMock.verify((Object[]) radioArray(handlers));
+    }
+
+    /**
+     * Verifies the mocks for the child radio buttons of the given handler.
+     *
+     * @param groupHandler the handler
+     */
+    private static void verifyGroup(RadioGroupWidgetHandler groupHandler)
+    {
+        verifyRadioHandlers(groupHandler.getRadioButtons());
+    }
+
+    /**
+     * Tests that the list with radio buttons cannot be manipulated.
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void testListOfRadioButtonsIsUnmodifiable()
+    {
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+
+        groupHandler.getRadioButtons().clear();
+    }
+
+    /**
+     * Tests that a defensive copy from the list with radio buttons passed to
+     * the constructor is made.
+     */
+    @Test
+    public void testDefensiveCopyIsCreatedFromConstructorParameter()
+    {
+        List<WidgetHandler> radios = createMockRadioHandlers();
+        List<WidgetHandler> orgRadios = new ArrayList<WidgetHandler>(radios);
+        RadioGroupWidgetHandler groupHandler = new RadioGroupWidgetHandler(
+                BUTTON_GROUP, radios, TIP_SEPARATOR);
+
+        radios.add(EasyMock.createMock(WidgetHandler.class));
+        assertEquals("List was manipulated", orgRadios,
+                groupHandler.getRadioButtons());
+    }
+
+    /**
+     * Tests whether the correct widget is returned.
+     */
+    @Test
+    public void testGetWidget()
+    {
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+
+        assertEquals("Wrong widget", BUTTON_GROUP, groupHandler.getWidget());
+    }
+
+    /**
+     * Tests whether the visibility state is correctly returned.
+     */
+    @Test
+    public void testIsVisible()
+    {
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        EasyMock.expect(firstRadio(groupHandler).isVisible())
+                .andReturn(Boolean.TRUE);
+        replayGroup(groupHandler);
+
+        assertTrue("Wrong result", groupHandler.isVisible());
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the visibility state can be set.
+     */
+    @Test
+    public void testSetVisible()
+    {
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        for (WidgetHandler h : groupHandler.getRadioButtons())
+        {
+            h.setVisible(false);
+        }
+        replayGroup(groupHandler);
+
+        groupHandler.setVisible(false);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the background color can be queried.
+     */
+    @Test
+    public void testGetBackgroundColor()
+    {
+        final Color color = Color.newRGBInstance(1, 2, 3);
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        EasyMock.expect(firstRadio(groupHandler).getBackgroundColor())
+                .andReturn(color);
+        replayGroup(groupHandler);
+
+        assertEquals("Wrong color", color, groupHandler.getBackgroundColor());
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the background color can be changed.
+     */
+    @Test
+    public void testSetBackgroundColor()
+    {
+        final Color color = Color.newRGBInstance(3, 2, 1);
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        for (WidgetHandler h : groupHandler.getRadioButtons())
+        {
+            h.setBackgroundColor(color);
+        }
+        replayGroup(groupHandler);
+
+        groupHandler.setBackgroundColor(color);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the foreground color can be queried.
+     */
+    @Test
+    public void testGetForegroundColor()
+    {
+        final Color color = Color.newRGBInstance(1, 2, 3);
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        EasyMock.expect(firstRadio(groupHandler).getForegroundColor())
+                .andReturn(color);
+        replayGroup(groupHandler);
+
+        assertEquals("Wrong color", color, groupHandler.getForegroundColor());
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the foreground color can be changed.
+     */
+    @Test
+    public void testSetForegroundColor()
+    {
+        final Color color = Color.newRGBInstance(3, 2, 1);
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        for (WidgetHandler h : groupHandler.getRadioButtons())
+        {
+            h.setForegroundColor(color);
+        }
+        replayGroup(groupHandler);
+
+        groupHandler.setForegroundColor(color);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the font can be queried.
+     */
+    @Test
+    public void testGetFont()
+    {
+        final Object font = "someFont";
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        EasyMock.expect(firstRadio(groupHandler).getFont()).andReturn(font);
+        replayGroup(groupHandler);
+
+        assertEquals("Wrong font", font, groupHandler.getFont());
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the font can be set.
+     */
+    @Test
+    public void testSetFont()
+    {
+        final Object font = "fontToSet";
+        RadioGroupWidgetHandler groupHandler = createGroupHandler();
+        for (WidgetHandler h : groupHandler.getRadioButtons())
+        {
+            h.setFont(font);
+        }
+        replayGroup(groupHandler);
+
+        groupHandler.setFont(font);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether two null tool tips can be combined.
+     */
+    @Test
+    public void testCombineToolTipsBothNull()
+    {
+        assertNull("Wrong result", RadioGroupWidgetHandler.combineToolTips(null,
+                null, TIP_SEPARATOR));
+    }
+
+    /**
+     * Tests combining tool tips if the second one is null.
+     */
+    @Test
+    public void testCombineToolTipsTip2Null()
+    {
+        final String tip = "ToolTip";
+
+        String combinedTip = RadioGroupWidgetHandler.combineToolTips(tip, null,
+                TIP_SEPARATOR);
+        assertEquals("Wrong combined tip", tip, combinedTip);
+    }
+
+    /**
+     * Tests combining tool tips if the first one is null.
+     */
+    @Test
+    public void testCombineToolTipsTip1Null()
+    {
+        final String tip = "ToolTip";
+
+        String combinedTip = RadioGroupWidgetHandler.combineToolTips(null, tip,
+                TIP_SEPARATOR);
+        assertEquals("Wrong combined tip", tip, combinedTip);
+    }
+
+    /**
+     * Tests whether two tool tips can be combined.
+     */
+    @Test
+    public void testCombineToolTipsNotNull()
+    {
+        final String tip1 = "firstTip";
+        final String tip2 = "secondTip";
+
+        String combinedTip = RadioGroupWidgetHandler.combineToolTips(tip1, tip2,
+                TIP_SEPARATOR);
+        assertEquals("Wrong combined tip", tip1 + TIP_SEPARATOR + tip2,
+                combinedTip);
+    }
+
+    /**
+     * Tests that a tool tip can be set for the group.
+     */
+    @Test
+    public void testSetToolTipInitially()
+    {
+        final String tip = "newToolTip";
+        WidgetHandler radio = EasyMock.createMock(WidgetHandler.class);
+        EasyMock.expect(radio.getToolTip()).andReturn(null);
+        radio.setToolTip(tip);
+        RadioGroupWidgetHandler groupHandler = new RadioGroupWidgetHandler(
+                BUTTON_GROUP, Collections.singleton(radio), TIP_SEPARATOR);
+        replayGroup(groupHandler);
+
+        groupHandler.setToolTip(tip);
+        assertEquals("Wrong group tip", tip, groupHandler.getToolTip());
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether the group tool tip can be changed multiple times.
+     */
+    @Test
+    public void testSetToolTipMultipleTimes()
+    {
+        final String radioTip = "This is my radio";
+        final String groupTip1 = "First group tip";
+        final String groupTip2 = "Second group tip";
+        WidgetHandler radio = EasyMock.createMock(WidgetHandler.class);
+        EasyMock.expect(radio.getToolTip()).andReturn(radioTip).anyTimes();
+        radio.setToolTip(radioTip + TIP_SEPARATOR + groupTip1);
+        radio.setToolTip(radioTip + TIP_SEPARATOR + groupTip2);
+        RadioGroupWidgetHandler groupHandler = new RadioGroupWidgetHandler(
+                BUTTON_GROUP, Collections.singleton(radio), TIP_SEPARATOR);
+        replayGroup(groupHandler);
+
+        groupHandler.setToolTip(groupTip1);
+        groupHandler.setToolTip(groupTip2);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests that a tool tip for the group can be set and removed again later.
+     */
+    @Test
+    public void testGroupToolTipCanBeRemoved()
+    {
+        final String radioTip1 = "This is my radio";
+        final String radioTip2 = "This is my other radio";
+        final String groupTip = "First group tip";
+        WidgetHandler radio1 = EasyMock.createMock(WidgetHandler.class);
+        WidgetHandler radio2 = EasyMock.createMock(WidgetHandler.class);
+        EasyMock.expect(radio1.getToolTip()).andReturn(radioTip1).anyTimes();
+        EasyMock.expect(radio2.getToolTip()).andReturn(radioTip2).anyTimes();
+        radio1.setToolTip(radioTip1 + TIP_SEPARATOR + groupTip);
+        radio1.setToolTip(radioTip1);
+        radio2.setToolTip(radioTip2 + TIP_SEPARATOR + groupTip);
+        radio2.setToolTip(radioTip2);
+        RadioGroupWidgetHandler groupHandler = new RadioGroupWidgetHandler(
+                BUTTON_GROUP, Arrays.asList(radio1, radio2), TIP_SEPARATOR);
+        replayGroup(groupHandler);
+
+        groupHandler.setToolTip(groupTip);
+        groupHandler.setToolTip(null);
+        verifyGroup(groupHandler);
+    }
+
+    /**
+     * Tests whether a change in the tool tip of the radio button is detected
+     * when setting the group tool tip.
+     */
+    @Test
+    public void testSetToolTipWithRadioTipChanged()
+    {
+        final String radio1Tip = "This is my radio (1)";
+        final String radio2Tip1 = "This is my other radio (1)";
+        final String radio2Tip2 = "This is my other radio (2)";
+        final String groupTip1 = "First group tip";
+        final String groupTip2 = "Second group tip";
+        WidgetHandler radio1 = EasyMock.createMock(WidgetHandler.class);
+        EasyMock.expect(radio1.getToolTip()).andReturn(radio1Tip).anyTimes();
+        WidgetHandler radio2 = EasyMock.createMock(WidgetHandler.class);
+        EasyMock.expect(radio2.getToolTip()).andReturn(radio2Tip1)
+                .andReturn(radio2Tip2);
+        radio1.setToolTip(radio1Tip + TIP_SEPARATOR + groupTip1);
+        radio1.setToolTip(radio1Tip + TIP_SEPARATOR + groupTip2);
+        radio2.setToolTip(radio2Tip1 + TIP_SEPARATOR + groupTip1);
+        radio2.setToolTip(radio2Tip2 + TIP_SEPARATOR + groupTip2);
+        RadioGroupWidgetHandler groupHandler = new RadioGroupWidgetHandler(
+                BUTTON_GROUP, Collections.singleton(radio1), TIP_SEPARATOR);
+        replayGroup(groupHandler);
+
+        groupHandler.setToolTip(groupTip1);
+        groupHandler.setToolTip(groupTip2);
+        verifyGroup(groupHandler);
+    }
+}
