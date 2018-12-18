8c76702db857619f96e2fdad92147e7cd907803d
oheger <oliver.heger@oliver-heger.de>
2018-12-18 21:51:42 +0100

    [46]: Added TextResource class.
    This is a useful helper class when dealing with texts that can be
    defined either directly or via a resource ID. For some properties of a
    file chooser dialog this is required.

---
+++ parent/core/src/main/java/net/sf/jguiraffe/gui/app/TextResource.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.app;
+
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * <p>
+ * A class representing a textual resource.
+ * </p>
+ * <p>
+ * This class can be used in scenarios where either a plain text or a text from
+ * the application's resources can be provided. Different factory methods for
+ * these use cases are provided. Instances can be queried for the data they
+ * contain. They also have a {@code resolve()} method which - given an
+ * {@link ApplicationContext} reference - returns the textual value of this
+ * resource.
+ * </p>
+ *
+ * @since 1.4
+ */
+public abstract class TextResource
+{
+    /**
+     * Constant for an undefined text resource. This instance returns
+     * <strong>null</strong> for all access methods.
+     */
+    public static final TextResource UNDEFINED = new TextResource()
+    {
+    };
+
+    /**
+     * Constructor preventing subclassing outside this package.
+     */
+    TextResource()
+    {
+    }
+
+    /**
+     * Returns the plain text for this resource. This method is defined if a
+     * plain text is available (i.e. the text does not have to be fetched from
+     * application resources). Otherwise, it returns <strong>null</strong>.
+     *
+     * @return the plain text for this resource or <strong>null</strong>
+     */
+    public String getPlainText()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the resource ID for this object. This method is defined if this
+     * resource represents an entry from the application's resources. Otherwise,
+     * result is <strong>null</strong>
+     *
+     * @return the resource ID or <strong>null</strong>
+     */
+    public Object getResourceID()
+    {
+        return null;
+    }
+
+    /**
+     * Resolves this resource and returns its text value. If necessary, does a
+     * resource lookup using the given application context. The final text
+     * represented by this object is returned. For an undefined resource result
+     * is <strong>null</strong>.
+     *
+     * @param applicationContext the application context
+     * @return the text represented by this resource
+     */
+    public String resolveText(ApplicationContext applicationContext)
+    {
+        return null;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder().append(getPlainText())
+                .append(getResourceID()).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (!(obj instanceof TextResource))
+        {
+            return false;
+        }
+
+        TextResource c = (TextResource) obj;
+        return ((getPlainText() == null) ? c.getPlainText() == null
+                : getPlainText().equals(c.getPlainText()))
+                && ((getResourceID() == null) ? c.getResourceID() == null
+                        : getResourceID().equals(c.getResourceID()));
+    }
+
+    /**
+     * Returns a string representation of this object. The string contains the
+     * data that is defined: the plain text or the resource ID. (For the
+     * undefined resource, neither is available.)
+     *
+     * @return a string for this object
+     */
+    @Override
+    public String toString()
+    {
+        StringBuilder buf = new StringBuilder();
+        buf.append("TextResource{");
+        if (getPlainText() != null)
+        {
+            buf.append("text='").append(getPlainText()).append("'");
+        }
+        if (getResourceID() != null)
+        {
+            buf.append("resourceID='").append(getResourceID()).append("'");
+        }
+        buf.append('}');
+        return buf.toString();
+    }
+
+    /**
+     * Creates a new instance of {@code TextResource} that represents the given
+     * plain text. No resource lookup is necessary; the text is directly
+     * provided. If the text is <strong>null</strong>, however, an undefined
+     * resource is returned.
+     *
+     * @param text the plain text
+     * @return a {@code TextResource} representing this plain text
+     */
+    public static TextResource fromText(String text)
+    {
+        return (text != null) ? new PlainTextResource(text) : UNDEFINED;
+    }
+
+    /**
+     * Creates a new instance of {@code TextResource} that represents an
+     * application resource with the given ID. The actual text is obtained via a
+     * resource lookup in the {@code resolve()} method. If the resource ID is
+     * <strong>null</strong>, however, an undefined resource is returned.
+     *
+     * @param resID the resource ID
+     * @return a {@code TextResource} representing this resource ID
+     */
+    public static TextResource fromResourceID(Object resID)
+    {
+        return (resID != null) ? new ResIDTextResource(resID) : UNDEFINED;
+    }
+
+    /**
+     * A sub class for resources that have a defined plain text.
+     */
+    private static class PlainTextResource extends TextResource
+    {
+        /** The plain text. */
+        private final String plainText;
+
+        /**
+         * Creates a new instance of {@code PlainTextResource} with the given
+         * plain text.
+         *
+         * @param text the plain text
+         */
+        PlainTextResource(String text)
+        {
+            plainText = text;
+        }
+
+        @Override
+        public String getPlainText()
+        {
+            return plainText;
+        }
+
+        @Override
+        public String resolveText(ApplicationContext applicationContext)
+        {
+            return getPlainText();
+        }
+    }
+
+    /**
+     * A sub class for resources that reference a resource ID.
+     */
+    private static class ResIDTextResource extends TextResource
+    {
+        /** The resource ID. */
+        private final Object resourceID;
+
+        /**
+         * Creates a new instance of {@code ResIDTextResource} with the given
+         * resource ID.
+         *
+         * @param resID the resource ID
+         */
+        ResIDTextResource(Object resID)
+        {
+            resourceID = resID;
+        }
+
+        @Override
+        public Object getResourceID()
+        {
+            return resourceID;
+        }
+
+        @Override
+        public String resolveText(ApplicationContext applicationContext)
+        {
+            return applicationContext.getResourceText(getResourceID());
+        }
+    }
+}
---
+++ parent/core/src/test/java/net/sf/jguiraffe/gui/app/TestTextResource.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.app;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import net.sf.jguiraffe.JGuiraffeTestHelper;
+
+/**
+ * Test class for {@code TextResource}.
+ */
+public class TestTextResource
+{
+    /**
+     * Tests that the given resource is undefined.
+     *
+     * @param resource the resource to be checked
+     */
+    private static void checkUndefinedResource(TextResource resource)
+    {
+        assertNull("Got plain text", resource.getPlainText());
+        assertNull("Got a resource ID", resource.getResourceID());
+
+        ApplicationContext appCtx =
+                EasyMock.createMock(ApplicationContext.class);
+        EasyMock.replay(appCtx);
+        assertNull("Got resolved text", resource.resolveText(appCtx));
+    }
+
+    /**
+     * Tests the constant for the undefined resource.
+     */
+    @Test
+    public void testUndefinedResource()
+    {
+        checkUndefinedResource(TextResource.UNDEFINED);
+    }
+
+    /**
+     * Tests a resource with a plain text.
+     */
+    @Test
+    public void testTextResource()
+    {
+        String text = "my plain text";
+        TextResource textResource = TextResource.fromText(text);
+
+        assertEquals("Wrong plain text", text, textResource.getPlainText());
+        assertNull("Got a resource ID", textResource.getResourceID());
+        assertEquals("Wrong resolved text", text,
+                textResource.resolveText(null));
+    }
+
+    /**
+     * Tests that an undefined resource is returned when a null text is passed
+     * in.
+     */
+    @Test
+    public void testFromNullText()
+    {
+        TextResource resource = TextResource.fromText(null);
+
+        checkUndefinedResource(resource);
+        assertSame("Wrong undefined resource", resource,
+                TextResource.UNDEFINED);
+    }
+
+    /**
+     * Tests whether a resource can be created from a resource ID.
+     */
+    @Test
+    public void testResourceIDResource()
+    {
+        final Object resID = "myResourceID";
+        final String resTxt = "Text for resource ID";
+        TextResource textResource = TextResource.fromResourceID(resID);
+
+        assertNull("Got plain text", textResource.getPlainText());
+        assertEquals("Wrong resource ID", resID, textResource.getResourceID());
+        ApplicationContext appCtx =
+                EasyMock.createMock(ApplicationContext.class);
+        EasyMock.expect(appCtx.getResourceText(resID)).andReturn(resTxt);
+        EasyMock.replay(appCtx);
+        assertEquals("Wrong resolved text", resTxt,
+                textResource.resolveText(appCtx));
+    }
+
+    /**
+     * Tests the the undefined resource is returned when a null resource ID is
+     * passed in.
+     */
+    @Test
+    public void testFromNullResourceID()
+    {
+        TextResource resource = TextResource.fromResourceID(null);
+
+        checkUndefinedResource(resource);
+        assertSame("Wrong undefined resource", resource,
+                TextResource.UNDEFINED);
+    }
+
+    /**
+     * Tests equals() if the expected result is true.
+     */
+    @Test
+    public void testEqualsTrue()
+    {
+        final String text = "A plain text";
+        final Object resID = "Some resource ID";
+        JGuiraffeTestHelper.checkEquals(TextResource.UNDEFINED,
+                TextResource.UNDEFINED, true);
+
+        TextResource resTxt1 = TextResource.fromText(text);
+        TextResource resTxt2 = TextResource.fromText(text);
+        JGuiraffeTestHelper.checkEquals(resTxt1, resTxt2, true);
+
+        TextResource res1 = TextResource.fromResourceID(resID);
+        TextResource res2 = TextResource.fromResourceID(resID);
+        JGuiraffeTestHelper.checkEquals(res1, res2, true);
+    }
+
+    /**
+     * Tests equals() if the expected result is false.
+     */
+    @Test
+    public void testEqualsFalse()
+    {
+        TextResource resTxt = TextResource.fromText("some text");
+        TextResource resId = TextResource.fromResourceID(new Object());
+
+        JGuiraffeTestHelper.checkEquals(resTxt, TextResource.UNDEFINED, false);
+        JGuiraffeTestHelper.checkEquals(resId, TextResource.UNDEFINED, false);
+        JGuiraffeTestHelper.checkEquals(resTxt, resId, false);
+        JGuiraffeTestHelper.checkEquals(resTxt,
+                TextResource.fromText("another text"), false);
+        JGuiraffeTestHelper.checkEquals(resId,
+                TextResource.fromResourceID(new Object()), false);
+    }
+
+    /**
+     * Tests corner cases of the equals() implementation.
+     */
+    @Test
+    public void testEqualsOtherObjects()
+    {
+        TextResource resource = TextResource.fromText("some resource");
+
+        JGuiraffeTestHelper.checkEquals(resource, null, false);
+        JGuiraffeTestHelper.checkEquals(resource, this, false);
+    }
+
+    /**
+     * Tests the string representation of a plain text resource.
+     */
+    @Test
+    public void testToStringPlainText()
+    {
+        final String text = "The Text.";
+        TextResource resource = TextResource.fromText(text);
+
+        String s = resource.toString();
+        assertThat("Text not found", s, containsString("text='" + text + "'"));
+        assertThat("Got resource ID", s, not(containsString("resourceID")));
+    }
+
+    /**
+     * Tests the string representation if a resource ID is defined.
+     */
+    @Test
+    public void testToStringResID()
+    {
+        final Object resID = 321;
+        TextResource resource = TextResource.fromResourceID(resID);
+
+        String s = resource.toString();
+        assertThat("ResID not found", s,
+                containsString("resourceID='" + resID + "'"));
+        assertThat("Got plain text", s, not(containsString("text=")));
+    }
+}
