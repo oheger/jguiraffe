515536a8f8507bbd34b0e6bb4c68c3d400f4d137
oheger <oliver.heger@oliver-heger.de>
2018-12-19 22:13:02 +0100

    [46]: Added DirectoryChooserOptions class.
    This class can be used to configure a dialog to choose a directory.
    Note that the properties supported so far are pretty minimalistic.
    Only the properties are available that are supported by the JavaFX
    implementation in Java 8.

---
+++ parent/core/src/main/java/net/sf/jguiraffe/gui/dlg/filechooser/DirectoryChooserOptions.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.dlg.filechooser;
+
+import java.io.File;
+
+import net.sf.jguiraffe.gui.dlg.AbstractDialogOptions;
+import net.sf.jguiraffe.gui.dlg.DialogResultCallback;
+
+/**
+ * <p>
+ * A class for defining the options of a directory chooser dialog.
+ * </p>
+ * <p>
+ * Instances of this class can be passed to a file chooser service to configure
+ * the behavior of a directory chooser dialog. The class offers a fluent API to
+ * set the single properties.
+ * </p>
+ * <p>
+ * Implementation note: Instances are not thread-safe.
+ * </p>
+ *
+ * @since 1.4
+ */
+public class DirectoryChooserOptions
+        extends AbstractDialogOptions<File, DirectoryChooserOptions>
+{
+    /** Stores the initial directory. */
+    private File initialDirectory;
+
+    /**
+     * Creates a new instance of {@code DirectoryChooserOptions} and sets the
+     * callback to be notified with the dialog result and an additional data
+     * object to be passed to the callback.
+     *
+     * @param resultCallback the result callback
+     * @param data the data object for the callback
+     * @param <D> the type of the data object
+     * @throws IllegalArgumentException if the result callback is
+     *         <strong>null</strong>
+     */
+    public <D> DirectoryChooserOptions(
+            DialogResultCallback<File, D> resultCallback, D data)
+    {
+        super(resultCallback, data);
+    }
+
+    /**
+     * Creates a new instance of {@code DirectoryChooserOptions} and sets the
+     * callback to be notified with the dialog result.
+     *
+     * @param resultCallback the result callback
+     * @throws IllegalArgumentException if the result callback is
+     *         <strong>null</strong>
+     */
+    public DirectoryChooserOptions(DialogResultCallback<File, ?> resultCallback)
+    {
+        super(resultCallback);
+    }
+
+    /**
+     * Allows setting the initial directory for the directory chooser dialog.
+     *
+     * @param directory the initial directory
+     * @return this object
+     */
+    public DirectoryChooserOptions setInitialDirectory(File directory)
+    {
+        this.initialDirectory = directory;
+        return getSelf();
+    }
+
+    /**
+     * Returns the initial directory for the directory chooser dialog. Result
+     * can be <strong>null</strong> if no such directory has been set.
+     *
+     * @return the initial directory
+     */
+    public File getInitialDirectory()
+    {
+        return initialDirectory;
+    }
+
+    protected DirectoryChooserOptions getSelf()
+    {
+        return this;
+    }
+}
---
+++ parent/core/src/test/java/net/sf/jguiraffe/gui/dlg/filechooser/TestDirectoryChooserOptions.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.dlg.filechooser;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import net.sf.jguiraffe.gui.dlg.DialogResultCallback;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+/**
+ * Test class for {@code DirectoryChooserOptions}.
+ */
+public class TestDirectoryChooserOptions
+{
+    /**
+     * Creates a mock for a result callback.
+     *
+     * @return the mock result callback
+     */
+    private static DialogResultCallback<File, String> createResultCallbackMock()
+    {
+        @SuppressWarnings("unchecked")
+        DialogResultCallback<File, String> callback =
+                EasyMock.createMock(DialogResultCallback.class);
+        return callback;
+    }
+
+    /**
+     * Tests that the callback and the data object are correctly passed to the
+     * super constructor.
+     */
+    @Test
+    public void testResultCallbackAndDataObjectArePassedToSuper()
+    {
+        final File resultFile = new File("myResult.dat");
+        final String data = "some context data";
+        DialogResultCallback<File, String> callback =
+                createResultCallbackMock();
+        callback.onDialogResult(resultFile, data);
+        EasyMock.replay(callback);
+
+        DirectoryChooserOptions options =
+                new DirectoryChooserOptions(callback, data);
+        options.getResultCallback().onDialogResult(resultFile, null);
+        EasyMock.verify(callback);
+    }
+
+    /**
+     * Tests whether the result callback (without a data object) is passed to
+     * the super constructor.
+     */
+    @Test
+    public void testResultCallbackIsPassedToSuper()
+    {
+        final File resultFile = new File("resultWithoutData.txt");
+        DialogResultCallback<File, String> callback =
+                createResultCallbackMock();
+        callback.onDialogResult(resultFile, null);
+        EasyMock.replay(callback);
+
+        DirectoryChooserOptions options = new DirectoryChooserOptions(callback);
+        options.getResultCallback().onDialogResult(resultFile, null);
+        EasyMock.verify(callback);
+    }
+
+    /**
+     * Tests whether the directory can be initialized.
+     */
+    @Test
+    public void testInitialDirectoryCanBeSet()
+    {
+        final File initDir = new File("start-dir");
+
+        DirectoryChooserOptions options =
+                new DirectoryChooserOptions(createResultCallbackMock())
+                        .setTitle("Select the dir")
+                        .setInitialDirectory(initDir);
+        assertEquals("Wrong initial directory", initDir,
+                options.getInitialDirectory());
+    }
+}
