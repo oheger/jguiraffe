9ec883458614b0ab43bcd161779bcc54353d19e7
oheger <oliver.heger@oliver-heger.de>
2018-12-21 21:23:59 +0100

    [46]: Added FileExtensionFilter class.
    This class represents a filter that restricts the files visible in a
    file chooser dialog to specific file extensions.

---
+++ parent/core/src/main/java/net/sf/jguiraffe/gui/dlg/filechooser/FileExtensionFilter.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.dlg.filechooser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import net.sf.jguiraffe.gui.app.TextResource;
+
+/**
+ * <p>
+ * A class representing an extension filter that can be used together with the
+ * file chooser service.
+ * </p>
+ * <p>
+ * When defining the options to show a file chooser dialog an arbitrary number
+ * of objects of this class can be specified. Each filter represents a specific
+ * file type and can be configured with the corresponding file extensions, such
+ * as {@code "Image files"; "gif", "jpeg", "jpg", "png"}. The user is displayed
+ * a list with all pre-configured extension filters; when selecting a specific
+ * one, only the files matched by the filter are visible. (When providing a file
+ * extension do not prepend a separator character or a wildcard, such as
+ * {@code *.txt}; just use the plain extension {@code txt}).
+ * </p>
+ * <p>
+ * The description of the filter is displayed to the user. It can be defined as
+ * a {@code TextResource} object, i.e. either as plain text or via a resource
+ * ID.
+ * </p>
+ * <p>
+ * Instances of this class are immutable.
+ * </p>
+ *
+ * @since 1.4
+ */
+public final class FileExtensionFilter
+{
+    /**
+     * Constant for a special extension with the meaning that all files should
+     * be matched. This extension should be specified for an "All files"
+     * filter.
+     */
+    public static final String EXT_ALL_FILES = "*";
+
+    /** Stores the resource for the filter description. */
+    private final TextResource description;
+
+    /** Stores the file extensions for this filter. */
+    private final List<String> extensions;
+
+    /**
+     * Creates a new instance of {@code FileExtensionFilter} and sets the
+     * resource for a description and a collection with the file extensions to
+     * be matched by this filter.
+     *
+     * @param descResource the resource for the filter description
+     * @param extList a list with file extensions
+     */
+    public FileExtensionFilter(TextResource descResource, List<String> extList)
+    {
+        description = descResource;
+        extensions =
+                Collections.unmodifiableList(new ArrayList<String>(extList));
+    }
+
+    /**
+     * Creates a new instance of {@code FileExtensionFilter} and sets the
+     * resource for a description and the single file extensions to be matched
+     * by this filter.
+     *
+     * @param descResource the resource for the filter description
+     * @param extensions the file extensions
+     */
+    public FileExtensionFilter(TextResource descResource, String... extensions)
+    {
+        this(descResource, Arrays.asList(extensions));
+    }
+
+    /**
+     * Returns the {@code TextResource} for the filter description.
+     *
+     * @return the resource for the filter description
+     */
+    public TextResource getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * Returns a (unmodifiable) list with the file extensions assigned to this
+     * filter.
+     *
+     * @return a collection with file extensions
+     */
+    public List<String> getExtensions()
+    {
+        return extensions;
+    }
+
+    /**
+     * {@inheritDoc} Two instances of this class are considered equals if all of
+     * their properties are equal.
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (!(o instanceof FileExtensionFilter))
+        {
+            return false;
+        }
+
+        FileExtensionFilter filter = (FileExtensionFilter) o;
+
+        if (!getDescription().equals(filter.getDescription()))
+            return false;
+        return getExtensions().equals(filter.getExtensions());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = getDescription().hashCode();
+        result = 31 * result + getExtensions().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "FileExtensionFilter{" + "description=" + description
+                + ", extensions=" + extensions + '}';
+    }
+}
---
+++ parent/core/src/test/java/net/sf/jguiraffe/gui/dlg/filechooser/TestFileExtensionFilter.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.dlg.filechooser;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import net.sf.jguiraffe.JGuiraffeTestHelper;
+import net.sf.jguiraffe.gui.app.TextResource;
+import org.junit.Test;
+
+/**
+ * Test class for {@code FileExtensionFilter}.
+ */
+public class TestFileExtensionFilter
+{
+    /** A test resource for the filter description. */
+    private static final TextResource DESC = TextResource.fromText("My filter");
+
+    /**
+     * Tests the management of the description resource.
+     */
+    @Test
+    public void testDescriptionResourceCanBeSetAndQueried()
+    {
+        FileExtensionFilter filter =
+                new FileExtensionFilter(DESC, Collections.singletonList("foo"));
+
+        assertEquals("Wrong description resource", DESC,
+                filter.getDescription());
+    }
+
+    /**
+     * Tests the constructor that expects a collection with extensions.
+     */
+    @Test
+    public void testInitFromExtensionCollection()
+    {
+        List<String> extensions = Arrays.asList("foo", "bar", "baz");
+
+        FileExtensionFilter filter = new FileExtensionFilter(DESC, extensions);
+        assertEquals("Wrong extensions", extensions, filter.getExtensions());
+    }
+
+    /**
+     * Tests that the constructor creates a defensive copy of the list with file
+     * extensions.
+     */
+    @Test
+    public void testExtensionListIsCopiedByConstructor()
+    {
+        List<String> extensionsOrg = Arrays.asList("ext1", "ext2");
+        List<String> extensions = new ArrayList<String>(extensionsOrg);
+        FileExtensionFilter filter = new FileExtensionFilter(DESC, extensions);
+
+        extensions.add("more");
+        assertEquals("Extensions were changed", extensionsOrg,
+                filter.getExtensions());
+    }
+
+    /**
+     * Tests that getExtensions() returns an unmodifiable list.
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void testExtensionListCannotBeModified()
+    {
+        FileExtensionFilter filter =
+                new FileExtensionFilter(DESC, Arrays.asList("a", "b", "c"));
+        List<String> extensions = filter.getExtensions();
+
+        extensions.add("more");
+    }
+
+    /**
+     * Tests the constructor expecting extensions as var args parameter.
+     */
+    @Test
+    public void testInitFromExtensionVarArg()
+    {
+        FileExtensionFilter filter = new FileExtensionFilter(DESC, "e1", "e2");
+
+        assertEquals("Wrong description", DESC, filter.getDescription());
+        assertEquals("Wrong extensions", Arrays.asList("e1", "e2"),
+                filter.getExtensions());
+    }
+
+    /**
+     * Tests equals() if the expected result is true.
+     */
+    @Test
+    public void testEqualsTrue()
+    {
+        FileExtensionFilter filter = new FileExtensionFilter(DESC, "tst", "eq");
+        FileExtensionFilter c = new FileExtensionFilter(
+                TextResource.fromText(DESC.getPlainText()),
+                filter.getExtensions());
+
+        JGuiraffeTestHelper.checkEquals(filter, c, true);
+    }
+
+    /**
+     * Tests equals() if the expected result is false.
+     */
+    @Test
+    public void testEqualsFalse()
+    {
+        List<String> extensions = Arrays.asList("doc", "dic", "dac", "duc");
+        FileExtensionFilter f1 = new FileExtensionFilter(DESC, extensions);
+
+        JGuiraffeTestHelper.checkEquals(f1, new FileExtensionFilter(
+                TextResource.fromText("other"), extensions), false);
+        List<String> otherExtensions = new ArrayList<String>(extensions);
+        otherExtensions.add("dec");
+        JGuiraffeTestHelper.checkEquals(f1,
+                new FileExtensionFilter(DESC, otherExtensions), false);
+    }
+
+    /**
+     * Tests equals() with some corner cases.
+     */
+    @Test
+    public void testEqualsOtherObjects()
+    {
+        FileExtensionFilter f = new FileExtensionFilter(DESC, "1", "2");
+
+        JGuiraffeTestHelper.testTrivialEquals(f);
+    }
+
+    /**
+     * Tests the string representation.
+     */
+    @Test
+    public void testToString()
+    {
+        List<String> extensions = Arrays.asList("mp3", "mp4", "mp5");
+        FileExtensionFilter filter = new FileExtensionFilter(DESC, extensions);
+
+        String s = filter.toString();
+        assertThat("No description", s, containsString("description=" + DESC));
+        assertThat("No extensions", s,
+                containsString("extensions=" + extensions));
+    }
+}
