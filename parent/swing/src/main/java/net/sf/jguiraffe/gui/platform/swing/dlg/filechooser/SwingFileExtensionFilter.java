491698b8ad4135499463031e902c5438ea2b187a
oheger <oliver.heger@oliver-heger.de>
2018-12-23 21:14:39 +0100

    [46]: Added SwingFileExtensionFilter class.
    This is a special implementation of the file filter interface used by
    Swing's JFileChooser for filtering files based on a file extension.
    
    (The Java 5 version of Swing does not have such an implementation.)

---
+++ parent/swing/src/main/java/net/sf/jguiraffe/gui/platform/swing/dlg/filechooser/SwingFileExtensionFilter.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.swing.dlg.filechooser;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import net.sf.jguiraffe.gui.app.ApplicationContext;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileExtensionFilter;
+
+/**
+ * <p>
+ * A specialized {@code FileFilter} implementation that implements the logic
+ * represented by a {@code FileExtensionFilter} to Swing's file chooser dialog.
+ * </p>
+ */
+class SwingFileExtensionFilter extends FileFilter
+{
+    /** The filter description. */
+    private final String description;
+
+    /** The set with file extensions accepted by this filter. */
+    private final Set<String> acceptedExtensions;
+
+    /** Flag whether all files should be accepted. */
+    private final boolean acceptAll;
+
+    /**
+     * Creates a new instance of {@code SwingFileExtensionFilter}.
+     *
+     * @param desc the description of this filter
+     * @param extensions a collection with the file extensions
+     */
+    public SwingFileExtensionFilter(String desc, Collection<String> extensions)
+    {
+        description = desc;
+        acceptedExtensions = new HashSet<String>();
+        for (String ext : extensions)
+        {
+            acceptedExtensions.add(ext.toLowerCase(Locale.ENGLISH));
+        }
+        acceptAll =
+                acceptedExtensions.contains(FileExtensionFilter.EXT_ALL_FILES);
+    }
+
+    /**
+     * Creates a new instance of {@code SwingFileExtensionFilter} that is
+     * equivalent to the given extension filter.
+     *
+     * @param filter the extension filter
+     * @param applicationContext the application context
+     * @return the new {@code SwingFileExtensionFilter} object
+     */
+    public static SwingFileExtensionFilter fromExtensionFilter(
+            FileExtensionFilter filter, ApplicationContext applicationContext)
+    {
+        return new SwingFileExtensionFilter(
+                filter.getDescription().resolveText(applicationContext),
+                filter.getExtensions());
+    }
+
+    /**
+     * {@inheritDoc} This implementation extracts the extension from the given
+     * file and checks whether it is contained in the set of accepted file
+     * extensions. If this instance is the <em>accepts all</em> filter, of
+     * course all files are accepted.
+     */
+    public boolean accept(File f)
+    {
+        if (acceptAll)
+        {
+            return true;
+        }
+        String extension = extractExtension(f);
+        return acceptedExtensions.contains(extension);
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (!(o instanceof SwingFileExtensionFilter))
+        {
+            return false;
+        }
+
+        SwingFileExtensionFilter filter = (SwingFileExtensionFilter) o;
+
+        if (getDescription() != null
+                ? !getDescription().equals(filter.getDescription())
+                : filter.getDescription() != null)
+            return false;
+        return acceptedExtensions.equals(filter.acceptedExtensions);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = getDescription() != null ? getDescription().hashCode() : 0;
+        result = 31 * result + acceptedExtensions.hashCode();
+        return result;
+    }
+
+    /**
+     * Extracts the extension from the given file in a form that it can be
+     * compared against the set of accepted file extensions.
+     *
+     * @param file the file
+     * @return the extension of this file
+     */
+    private static String extractExtension(File file)
+    {
+        String name = file.getName();
+        int posExt = name.lastIndexOf('.');
+        return (posExt >= 0)
+                ? name.substring(posExt + 1).toLowerCase(Locale.ENGLISH)
+                : "";
+    }
+}
---
+++ parent/swing/src/test/java/net/sf/jguiraffe/gui/platform/swing/dlg/filechooser/TestSwingFileExtensionFilter.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.swing.dlg.filechooser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.sf.jguiraffe.JGuiraffeTestHelper;
+import net.sf.jguiraffe.gui.app.ApplicationContext;
+import net.sf.jguiraffe.gui.app.TextResource;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileExtensionFilter;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+/**
+ * Test class for {@code SwingFileExtensionFilter}.
+ */
+public class TestSwingFileExtensionFilter
+{
+    /**
+     * Tests whether the filter description is resolved correctly.
+     */
+    @Test
+    public void testDescriptionIsResolved()
+    {
+        final Object resID = "myResourceID";
+        final String description = "This is my filter";
+        ApplicationContext appCtx =
+                EasyMock.createMock(ApplicationContext.class);
+        EasyMock.expect(appCtx.getResourceText(resID)).andReturn(description);
+        EasyMock.replay(appCtx);
+        FileExtensionFilter filter =
+                new FileExtensionFilter(TextResource.fromResourceID(resID));
+
+        SwingFileExtensionFilter swingFilter =
+                SwingFileExtensionFilter.fromExtensionFilter(filter, appCtx);
+        assertEquals("Wrong description", description,
+                swingFilter.getDescription());
+    }
+
+    /**
+     * Tests the accept() method if the expected result is true.
+     */
+    @Test
+    public void testAcceptTrue()
+    {
+        File[] files = new File[] {
+                new File("image1.jpg"), new File("image2.JPG"),
+                new File("image3.jpeg"), new File("image.3.jpg"),
+                new File("test/imageSub.JpEg")
+        };
+        FileExtensionFilter filter =
+                new FileExtensionFilter(TextResource.UNDEFINED, "jpg", "jpeg");
+
+        SwingFileExtensionFilter swingFilter =
+                SwingFileExtensionFilter.fromExtensionFilter(filter,
+                        EasyMock.createMock(ApplicationContext.class));
+        for (File file : files)
+        {
+            assertTrue("Not accepted: " + file, swingFilter.accept(file));
+        }
+    }
+
+    /**
+     * Tests the accept() method if the expected result is false.
+     */
+    @Test
+    public void testAcceptFalse()
+    {
+        File[] files = new File[] {
+                new File("image1.gif"), new File("image2.jpge"),
+                new File("strange_image"), new File("strange2.")
+        };
+        FileExtensionFilter filter =
+                new FileExtensionFilter(TextResource.UNDEFINED, "jpg", "jpeg");
+
+        SwingFileExtensionFilter swingFilter =
+                SwingFileExtensionFilter.fromExtensionFilter(filter,
+                        EasyMock.createMock(ApplicationContext.class));
+        for (File file : files)
+        {
+            assertFalse("Accepted: " + file, swingFilter.accept(file));
+        }
+    }
+
+    /**
+     * Tests whether an accepts all filter works as expected.
+     */
+    @Test
+    public void testAcceptAll()
+    {
+        FileExtensionFilter filter = new FileExtensionFilter(
+                TextResource.UNDEFINED, FileExtensionFilter.EXT_ALL_FILES);
+        SwingFileExtensionFilter swingFilter =
+                SwingFileExtensionFilter.fromExtensionFilter(filter,
+                        EasyMock.createMock(ApplicationContext.class));
+
+        assertTrue("Not accepted (1)",
+                swingFilter.accept(new File("test.jpg")));
+        assertTrue("Not accepted (2)", swingFilter.accept(new File("test")));
+    }
+
+    /**
+     * Tests equals() if the expected result is true.
+     */
+    @Test
+    public void testEqualsTrue()
+    {
+        SwingFileExtensionFilter filter1 = new SwingFileExtensionFilter("desc",
+                Collections.singleton("jpg"));
+        SwingFileExtensionFilter filter2 = new SwingFileExtensionFilter(
+                filter1.getDescription(), Collections.singleton("JPG"));
+        JGuiraffeTestHelper.checkEquals(filter1, filter2, true);
+
+        SwingFileExtensionFilter filter3 =
+                new SwingFileExtensionFilter(null, new HashSet<String>());
+        SwingFileExtensionFilter filter4 =
+                new SwingFileExtensionFilter(null, new HashSet<String>());
+        JGuiraffeTestHelper.checkEquals(filter3, filter4, true);
+    }
+
+    /**
+     * Tests equals() if the expected result is false.
+     */
+    @Test
+    public void testEqualsFalse()
+    {
+        Set<String> extensions = Collections.singleton("mp3");
+        SwingFileExtensionFilter filter =
+                new SwingFileExtensionFilter("filterDesc", extensions);
+
+        JGuiraffeTestHelper.checkEquals(filter, new SwingFileExtensionFilter(
+                filter.getDescription(), new HashSet<String>()), false);
+        JGuiraffeTestHelper.checkEquals(filter,
+                new SwingFileExtensionFilter("other", extensions), false);
+    }
+
+    /**
+     * Tests corner cases of the equals() implementation.
+     */
+    @Test
+    public void testEqualsOtherObjects()
+    {
+        JGuiraffeTestHelper.testTrivialEquals(new SwingFileExtensionFilter(
+                "myFilter", Collections.singleton("foo")));
+    }
+}
