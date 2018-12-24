163b1db76b318bcf0b56d08407e6d578cb18815c
oheger <oliver.heger@oliver-heger.de>
2018-12-24 17:48:46 +0100

    [46]: Created Swing implementation of FileChooserDialogService.
    This service implementation makes use of Swing's JFileChooser class to
    display dialogs to select files.

---
+++ parent/swing/src/main/java/net/sf/jguiraffe/gui/platform/swing/dlg/filechooser/SwingFileChooserDialogService.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.swing.dlg.filechooser;
+
+import java.awt.Component;
+import java.io.File;
+import java.util.Arrays;
+
+import javax.swing.JFileChooser;
+
+import net.sf.jguiraffe.gui.app.ApplicationContext;
+import net.sf.jguiraffe.gui.dlg.AbstractDialogOptions;
+import net.sf.jguiraffe.gui.dlg.filechooser.AbstractFileChooserOptions;
+import net.sf.jguiraffe.gui.dlg.filechooser.DirectoryChooserOptions;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileChooserDialogService;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileChooserOptions;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileExtensionFilter;
+import net.sf.jguiraffe.gui.dlg.filechooser.MultiFileChooserOptions;
+import net.sf.jguiraffe.gui.platform.swing.builder.window.SwingWindow;
+
+/**
+ * <p>
+ * The Swing-specific implementation of the file chooser dialog service.
+ * </p>
+ * <p>
+ * This implementation uses the {@code JFileChooser} class of Swing to display
+ * dialogs to choose files or directories.
+ * </p>
+ *
+ * @since 1.4
+ */
+public class SwingFileChooserDialogService implements FileChooserDialogService
+{
+    /** Stores the application context. */
+    private final ApplicationContext applicationContext;
+
+    /**
+     * Creates a new instance of {@code SwingFileChooserDialogService} and
+     * initializes it with the given application context.
+     *
+     * @param appCtx the application context
+     */
+    public SwingFileChooserDialogService(ApplicationContext appCtx)
+    {
+        applicationContext = appCtx;
+    }
+
+    /**
+     * Returns the {@code ApplicationContext} used by service.
+     *
+     * @return the {@code ApplicationContext}
+     */
+    public ApplicationContext getApplicationContext()
+    {
+        return applicationContext;
+    }
+
+    public void showOpenFileDialog(FileChooserOptions options)
+    {
+        JFileChooser fileChooser =
+                createFileChooserForFileSelection(options, false);
+        if (fileChooser.showOpenDialog(
+                getParentWindow()) == JFileChooser.APPROVE_OPTION)
+        {
+            handleDialogResult(options, fileChooser.getSelectedFile());
+        }
+        else
+        {
+            handleDialogCanceled(options);
+        }
+    }
+
+    public void showOpenMultiFileDialog(MultiFileChooserOptions options)
+    {
+        JFileChooser fileChooser =
+                createFileChooserForFileSelection(options, true);
+        if (fileChooser.showOpenDialog(
+                getParentWindow()) == JFileChooser.APPROVE_OPTION)
+        {
+            handleDialogResult(options,
+                    Arrays.asList(fileChooser.getSelectedFiles()));
+        }
+        else
+        {
+            handleDialogCanceled(options);
+        }
+    }
+
+    public void showSaveFileDialog(FileChooserOptions options)
+    {
+        JFileChooser fileChooser =
+                createFileChooserForFileSelection(options, false);
+        if (fileChooser.showSaveDialog(
+                getParentWindow()) == JFileChooser.APPROVE_OPTION)
+        {
+            handleDialogResult(options, fileChooser.getSelectedFile());
+        }
+        else
+        {
+            handleDialogCanceled(options);
+        }
+    }
+
+    public void showChooseDirectoryDialog(DirectoryChooserOptions options)
+    {
+        JFileChooser fileChooser =
+                initInitialDirectory(createInitializedFileChooser(options),
+                        options.getInitialDirectory());
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (fileChooser.showOpenDialog(
+                getParentWindow()) == JFileChooser.APPROVE_OPTION)
+        {
+            handleDialogResult(options, fileChooser.getSelectedFile());
+        }
+        else
+        {
+            handleDialogCanceled(options);
+        }
+    }
+
+    /**
+     * Creates the {@code JFileChooser} object that is used to display the
+     * dialogs constructed by this class.
+     *
+     * @return the {@code JFileChooser}
+     */
+    protected JFileChooser createFileChooser()
+    {
+        return new JFileChooser();
+    }
+
+    /**
+     * Creates a {@code JFileChooser} object and initializes it with default
+     * settings from the given options object.
+     *
+     * @param options the dialog options
+     * @return the initialized {@code JFileChooser}
+     */
+    private JFileChooser createInitializedFileChooser(
+            AbstractDialogOptions<?, ?> options)
+    {
+        JFileChooser fileChooser = createFileChooser();
+        fileChooser
+                .setDialogTitle(options.resolveTitle(getApplicationContext()));
+        return fileChooser;
+    }
+
+    /**
+     * Creates an instance of {@code JFileChooser} and initializes it for
+     * selecting a file. This method takes care about the initialization of the
+     * properties determined by an {@code AbstractFileChooserOptions} instance.
+     *
+     * @param options the options
+     * @param multiSelection flag for multi file selection
+     * @return the initialized {@code JFileChooser}
+     */
+    private JFileChooser createFileChooserForFileSelection(
+            AbstractFileChooserOptions<?, ?> options, boolean multiSelection)
+    {
+        JFileChooser fileChooser =
+                initInitialDirectory(createInitializedFileChooser(options),
+                        options.getInitialDirectory());
+        if (options.getCurrentFile() != null)
+        {
+            fileChooser.setSelectedFile(options.getCurrentFile());
+        }
+        fileChooser.setMultiSelectionEnabled(multiSelection);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        for (FileExtensionFilter filter : options.getFilters())
+        {
+            fileChooser.addChoosableFileFilter(SwingFileExtensionFilter
+                    .fromExtensionFilter(filter, getApplicationContext()));
+        }
+        return fileChooser;
+    }
+
+    /**
+     * Initializes a file chooser's initial directory if it is defined.
+     *
+     * @param fileChooser the file chooser
+     * @param directory the directory (can be <strong>null</strong>)
+     * @return the same file chooser reference
+     */
+    private JFileChooser initInitialDirectory(JFileChooser fileChooser,
+            File directory)
+    {
+        if (directory != null)
+        {
+            fileChooser.setCurrentDirectory(directory);
+        }
+        return fileChooser;
+    }
+
+    /**
+     * Returns the parent window from the application context.
+     *
+     * @return the parent window
+     */
+    private Component getParentWindow()
+    {
+        return ((SwingWindow) getApplicationContext().getMainWindow())
+                .getComponent();
+    }
+
+    /**
+     * Reacts on a dialog closed by OK by passing the given result to the
+     * callback defined in the options.
+     *
+     * @param options the options
+     * @param result the dialog result
+     * @param <T> the type of the dialog result
+     */
+    private static <T> void handleDialogResult(
+            AbstractDialogOptions<T, ?> options, T result)
+    {
+        options.getResultCallback().onDialogResult(result, null);
+    }
+
+    /**
+     * Reacts on a canceled dialog by invoking the corresponding callback from
+     * the given options.
+     *
+     * @param options the options
+     */
+    private static void handleDialogCanceled(
+            AbstractDialogOptions<?, ?> options)
+    {
+        options.getCancelInvoker().run();
+    }
+}
---
+++ parent/swing/src/test/java/net/sf/jguiraffe/gui/platform/swing/dlg/filechooser/TestSwingFileChooserDialogService.java
@@ -0,0 +1,409 @@
+/*
+ * Copyright 2006-2018 The JGUIraffe Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jguiraffe.gui.platform.swing.dlg.filechooser;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.awt.Component;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import net.sf.jguiraffe.gui.app.TextResource;
+import net.sf.jguiraffe.gui.dlg.DialogCanceledCallback;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileChooserOptions;
+import net.sf.jguiraffe.gui.dlg.filechooser.FileExtensionFilter;
+import net.sf.jguiraffe.gui.dlg.filechooser.MultiFileChooserOptions;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sf.jguiraffe.gui.app.ApplicationContext;
+import net.sf.jguiraffe.gui.dlg.DialogResultCallback;
+import net.sf.jguiraffe.gui.dlg.filechooser.DirectoryChooserOptions;
+import net.sf.jguiraffe.gui.platform.swing.builder.window.SwingWindow;
+
+/**
+ * Test class for {@code SwingFileChooserDialogService}.
+ */
+public class TestSwingFileChooserDialogService
+{
+    /** The text for the title. */
+    private static final String TITLE = "Choose a file";
+
+    /** The context data for the result callback. */
+    private static final String DATA = "dialog context data";
+
+    /** A test extension filter. */
+    private static final FileExtensionFilter FILTER_IMAGES =
+            new FileExtensionFilter(TextResource.fromText("images"), "jpg",
+                    "gif");
+
+    /** Another extension filter. */
+    private static final FileExtensionFilter FILTER_MUSIC =
+            new FileExtensionFilter(TextResource.fromText("music"), "mp3",
+                    "ogg");
+
+    /** Mock for the application context. */
+    private ApplicationContext applicationContext;
+
+    /** The component representing the main window. */
+    private Component mainWindowComponent;
+
+    /** Mock for the main application window. */
+    private SwingWindow mainWindow;
+
+    /** Mock for the underlying file chooser. */
+    private JFileChooser fileChooser;
+
+    /** The instance to be tested. */
+    private SwingFileChooserDialogServiceTestImpl dialogService;
+
+    @Before
+    public void setUp()
+    {
+        applicationContext = EasyMock.createMock(ApplicationContext.class);
+        mainWindow = EasyMock.createNiceMock(SwingWindow.class);
+        fileChooser = EasyMock.createMock(JFileChooser.class);
+        mainWindowComponent = new JFrame();
+        EasyMock.expect(applicationContext.getMainWindow())
+                .andReturn(mainWindow);
+        EasyMock.expect(mainWindow.getComponent())
+                .andReturn(mainWindowComponent);
+        fileChooser.setDialogTitle(TITLE);
+        dialogService =
+                new SwingFileChooserDialogServiceTestImpl(applicationContext);
+    }
+
+    /**
+     * Creates a mock for a dialog result callback.
+     *
+     * @param <T> the type of the result
+     * @return the mock
+     */
+    private static <T> DialogResultCallback<T, String> createResultCallback()
+    {
+        @SuppressWarnings("unchecked")
+        DialogResultCallback<T, String> callback =
+                EasyMock.createMock(DialogResultCallback.class);
+        return callback;
+    }
+
+    /**
+     * Creates a mock for a dialog canceled callback.
+     *
+     * @return the callback mock
+     */
+    private static DialogCanceledCallback<String> createCanceledCallback()
+    {
+        @SuppressWarnings("unchecked")
+        DialogCanceledCallback<String> callback =
+                EasyMock.createMock(DialogCanceledCallback.class);
+        return callback;
+    }
+
+    /**
+     * Replays the mocks involved in this test and the given additional mocks.
+     *
+     * @param mocks additional mocks to be replayed
+     */
+    private void replayMocks(Object... mocks)
+    {
+        EasyMock.replay(applicationContext, mainWindow, fileChooser);
+        if (mocks.length > 0)
+        {
+            EasyMock.replay(mocks);
+        }
+    }
+
+    /**
+     * Tests whether a correct file chooser dialog is created.
+     */
+    @Test
+    public void testCorrectFileChooserIsCreated()
+    {
+        SwingFileChooserDialogService service =
+                new SwingFileChooserDialogService(applicationContext);
+
+        assertNotNull("No file chooser", service.createFileChooser());
+    }
+
+    /**
+     * Tests whether a directory chooser dialog can be opened.
+     */
+    @Test
+    public void testDirectoryChooserDialogCanBeOpened()
+    {
+        File initialDirectory = new File("initial");
+        File result = new File("result");
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        resultCallback.onDialogResult(result, DATA);
+        fileChooser.setCurrentDirectory(initialDirectory);
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        EasyMock.expect(fileChooser.getSelectedFile()).andReturn(result);
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.APPROVE_OPTION);
+        replayMocks(resultCallback);
+        DirectoryChooserOptions options =
+                new DirectoryChooserOptions(resultCallback, DATA)
+                        .setTitle(TITLE).setInitialDirectory(initialDirectory);
+
+        dialogService.showChooseDirectoryDialog(options);
+        EasyMock.verify(resultCallback, fileChooser);
+    }
+
+    /**
+     * Expects that the file filters have been applied to the file chooser.
+     */
+    private void expectFileFilters()
+    {
+        expectFileFilter(FILTER_IMAGES);
+        expectFileFilter(FILTER_MUSIC);
+    }
+
+    /**
+     * Expects that the given file filter is applied to the file chooser.
+     *
+     * @param filter the filter
+     */
+    private void expectFileFilter(FileExtensionFilter filter)
+    {
+        SwingFileExtensionFilter swingFilter = SwingFileExtensionFilter
+                .fromExtensionFilter(filter, applicationContext);
+        fileChooser.addChoosableFileFilter(swingFilter);
+    }
+
+    /**
+     * Tests whether a canceled directory dialog is handled correctly.
+     */
+    @Test
+    public void testDirectoryChooserCanceled()
+    {
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        DialogCanceledCallback<String> canceledCallback =
+                createCanceledCallback();
+        canceledCallback.onDialogCanceled(DATA);
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.CANCEL_OPTION);
+        replayMocks(resultCallback, canceledCallback);
+        DirectoryChooserOptions options =
+                new DirectoryChooserOptions(resultCallback).setTitle(TITLE)
+                        .setCanceledCallback(canceledCallback, DATA);
+
+        dialogService.showChooseDirectoryDialog(options);
+        EasyMock.verify(resultCallback, canceledCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether a dialog to open a single file can be displayed.
+     */
+    @Test
+    public void testOpenSingleFileDialog()
+    {
+        File initialDirectory = new File("initialDir");
+        File currentFile = new File("current.txt");
+        File result = new File("resultFile");
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        resultCallback.onDialogResult(result, DATA);
+        fileChooser.setCurrentDirectory(initialDirectory);
+        fileChooser.setSelectedFile(currentFile);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(false);
+        expectFileFilters();
+        EasyMock.expect(fileChooser.getSelectedFile()).andReturn(result);
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.APPROVE_OPTION);
+        replayMocks(resultCallback);
+        FileChooserOptions options =
+                new FileChooserOptions(resultCallback, DATA).setTitle(TITLE)
+                        .setInitialDirectory(initialDirectory)
+                        .setCurrentFile(currentFile)
+                        .setFilters(FILTER_IMAGES, FILTER_MUSIC);
+
+        dialogService.showOpenFileDialog(options);
+        EasyMock.verify(resultCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether a cancellation of the open single file dialog is handled.
+     */
+    @Test
+    public void testOpenSingleFileDialogCanceled()
+    {
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        DialogCanceledCallback<String> canceledCallback =
+                createCanceledCallback();
+        canceledCallback.onDialogCanceled(DATA);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(false);
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.CANCEL_OPTION);
+        FileChooserOptions options = new FileChooserOptions(resultCallback)
+                .setCanceledCallback(canceledCallback, DATA).setTitle(TITLE);
+        replayMocks(resultCallback, canceledCallback);
+
+        dialogService.showOpenFileDialog(options);
+        EasyMock.verify(canceledCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether a dialog to open multiple files can be displayed.
+     */
+    @Test
+    public void testOpenMultiFileDialog()
+    {
+        File initialDirectory = new File("initialDir");
+        File currentFile = new File("current.txt");
+        File result1 = new File("resultFile1.txt");
+        File result2 = new File("resultFile2.doc");
+        DialogResultCallback<List<File>, String> resultCallback =
+                createResultCallback();
+        resultCallback.onDialogResult(Arrays.asList(result1, result2), DATA);
+        fileChooser.setCurrentDirectory(initialDirectory);
+        fileChooser.setSelectedFile(currentFile);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(true);
+        expectFileFilters();
+        EasyMock.expect(fileChooser.getSelectedFiles()).andReturn(new File[] {
+                result1, result2
+        });
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.APPROVE_OPTION);
+        replayMocks(resultCallback);
+        MultiFileChooserOptions options =
+                new MultiFileChooserOptions(resultCallback, DATA)
+                        .setTitle(TITLE).setInitialDirectory(initialDirectory)
+                        .setCurrentFile(currentFile)
+                        .setFilters(FILTER_IMAGES, FILTER_MUSIC);
+
+        dialogService.showOpenMultiFileDialog(options);
+        EasyMock.verify(resultCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether a cancellation of a multi file open dialog is handled
+     * correctly.
+     */
+    @Test
+    public void testOpenMultiFileDialogCanceled()
+    {
+        DialogResultCallback<List<File>, String> resultCallback =
+                createResultCallback();
+        DialogCanceledCallback<String> canceledCallback =
+                createCanceledCallback();
+        canceledCallback.onDialogCanceled(DATA);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(true);
+        EasyMock.expect(fileChooser.showOpenDialog(mainWindowComponent))
+                .andReturn(JFileChooser.CANCEL_OPTION);
+        MultiFileChooserOptions options =
+                new MultiFileChooserOptions(resultCallback)
+                        .setCanceledCallback(canceledCallback, DATA)
+                        .setTitle(TITLE);
+        replayMocks(resultCallback, canceledCallback);
+
+        dialogService.showOpenMultiFileDialog(options);
+        EasyMock.verify(canceledCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether a dialog to select a file for saving can be displayed.
+     */
+    @Test
+    public void testShowSaveDialog()
+    {
+        File initialDirectory = new File("initialDir");
+        File currentFile = new File("current.txt");
+        File result = new File("resultFile.sav");
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        resultCallback.onDialogResult(result, DATA);
+        fileChooser.setCurrentDirectory(initialDirectory);
+        fileChooser.setSelectedFile(currentFile);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(false);
+        expectFileFilters();
+        EasyMock.expect(fileChooser.getSelectedFile()).andReturn(result);
+        EasyMock.expect(fileChooser.showSaveDialog(mainWindowComponent))
+                .andReturn(JFileChooser.APPROVE_OPTION);
+        replayMocks(resultCallback);
+        FileChooserOptions options =
+                new FileChooserOptions(resultCallback, DATA).setTitle(TITLE)
+                        .setInitialDirectory(initialDirectory)
+                        .setCurrentFile(currentFile)
+                        .setFilters(FILTER_IMAGES, FILTER_MUSIC);
+
+        dialogService.showSaveFileDialog(options);
+        EasyMock.verify(resultCallback, fileChooser);
+    }
+
+    /**
+     * Tests whether the cancellation of the file save dialog is handled.
+     */
+    @Test
+    public void testShowSaveDialogCanceled()
+    {
+        DialogResultCallback<File, String> resultCallback =
+                createResultCallback();
+        DialogCanceledCallback<String> canceledCallback =
+                createCanceledCallback();
+        canceledCallback.onDialogCanceled(DATA);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setMultiSelectionEnabled(false);
+        EasyMock.expect(fileChooser.showSaveDialog(mainWindowComponent))
+                .andReturn(JFileChooser.CANCEL_OPTION);
+        FileChooserOptions options = new FileChooserOptions(resultCallback)
+                .setCanceledCallback(canceledCallback, DATA).setTitle(TITLE);
+        replayMocks(resultCallback, canceledCallback);
+
+        dialogService.showSaveFileDialog(options);
+        EasyMock.verify(canceledCallback, fileChooser);
+    }
+
+    /**
+     * A test implementation of the dialog service. This implementation operates
+     * on a mock file chooser, so that the initializations on this object can be
+     * verified.
+     */
+    private class SwingFileChooserDialogServiceTestImpl
+            extends SwingFileChooserDialogService
+    {
+        public SwingFileChooserDialogServiceTestImpl(ApplicationContext appCtx)
+        {
+            super(appCtx);
+        }
+
+        @Override
+        protected JFileChooser createFileChooser()
+        {
+            return fileChooser;
+        }
+    }
+}
