c3cbd387c4cb82daf08148f91d6381d06e43150e
Oliver Heger <oliver.heger@oliver-heger.de>
2025-06-20 21:56:34 +0200

    JavaFx: Reworked parsing of stylesheets.
    The `Styles` class and the associated mechanism to parse stylesheets
    have been fully reworked. The new algorithm is more lenient, and it
    supports more specifics of CSS, e.g. string literals included in
    single quotes.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- parent/java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/widget/TestStyles.scala
+++ parent/java-fx/src/test/scala/net/sf/jguiraffe/gui/platform/javafx/builder/components/widget/TestStyles.scala
@@ -20,33 +20,34 @@ import org.junit.Test
 import org.scalatestplus.junit.JUnitSuite
 
 /**
- * Test class for ''Styles''.
- */
+  * Test class for ''Styles''.
+  */
 class TestStyles extends JUnitSuite {
   /** Constant for a textual styles definition. */
-  val StylesDefinition = """-fx-font: 16px   "Serif;Test";
+  val StylesDefinition =
+    """-fx-font: 16px   "Serif;Test";
         -fx-background-color :    #AA99FF;
         -fx-unknown: 18.5%_blupp http://test.com?check=x;"""
 
   /**
-   * Tests whether an empty instance can be created.
-   */
+    * Tests whether an empty instance can be created.
+    */
   @Test def testInitEmpty(): Unit = {
     val styles = Styles()
     assertTrue("Got styles", styles.styleKeys.isEmpty)
   }
 
   /**
-   * Tests whether an instance can be created from a styles definition.
-   */
+    * Tests whether an instance can be created from a styles definition.
+    */
   @Test def testInitFromDefinition(): Unit = {
     val styles = Styles(StylesDefinition)
     assertEquals("Wrong number of styles", 3, styles.styleKeys.size)
   }
 
   /**
-   * Tests whether the value of a style can be queried.
-   */
+    * Tests whether the value of a style can be queried.
+    */
   @Test def testQueryStyle(): Unit = {
     val styles = Styles(StylesDefinition)
     assertEquals("Wrong style value (1)", "#AA99FF",
@@ -56,17 +57,95 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether style sheet values with a string literal are parsed
-   * correctly and can be queried.
-   */
+    * Tests whether style sheet values with a string literal are parsed
+    * correctly and can be queried.
+    */
   @Test def testQueryStyleWithStringLiteral(): Unit = {
     val styles = Styles(StylesDefinition)
     assertEquals("Wrong style value", "16px \"Serif;Test\"", styles("-fx-font").get)
   }
 
+  @Test def testStringLiteralWithQuotedDoubleQuote(): Unit = {
+    val definition = "style: \"My \\\"value\";"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "\"My \\\"value\"", styles("style").get)
+  }
+
+  @Test def testStringLiteralContainingSingleQuote(): Unit = {
+    val definition = "style: \"It's difficult; right?\";"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "\"It's difficult; right?\"", styles("style").get)
+  }
+
+  @Test def testStringLiteralWithSingleQuotes(): Unit = {
+    val definition = "style: 'My value;';"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "'My value;'", styles("style").get)
+  }
+
+  @Test def testStringLiteralWithSingleQuotesAndQuotedQuote(): Unit = {
+    val definition = "style:'It\\'s difficult';"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "'It\\'s difficult'", styles("style").get)
+  }
+
+  @Test def testStringLiteralWithSingleQuotesContainingDoubleQuote(): Unit = {
+    val definition = "style: 'He said: \"Wow\"; and';"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "'He said: \"Wow\"; and'", styles("style").get)
+  }
+
+  @Test def testStringLiteralContainingBackslash(): Unit = {
+    val definition = "style: \"c:\\\\path;foo\";"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "\"c:\\\\path;foo\"", styles("style").get)
+  }
+
+  @Test def testStringLiteralWithSingleQuotesContainingBackslash(): Unit = {
+    val definition = "style: 'c:\\\\path;foo';"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "'c:\\\\path;foo'", styles("style").get)
+  }
+
+  @Test def testStringLiteralContainingTrailingBackslash(): Unit = {
+    val definition = "style: \"c:\\\\path\\\\\";"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "\"c:\\\\path\\\\\"", styles("style").get)
+  }
+
+  @Test def testStringLiteralContainingMultiWhitespace(): Unit = {
+    val definition = "style:'With a  long   whitespace ';"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "'With a  long   whitespace '", styles("style").get)
+  }
+
+  @Test def testDefinitionWithoutTerminalSemicolon(): Unit = {
+    val definition = "style:value"
+    val styles = Styles(definition)
+
+    assertEquals("Wrong style value", "value", styles("style").get)
+  }
+
+  @Test def testDefinitionIncludingEmptyStyle(): Unit = {
+    val definition = ";" + StylesDefinition
+    val styles = Styles(definition)
+
+    assertEquals("Wrong number of styles", 3, styles.styleKeys.size)
+    assertEquals("Wrong style value", "#AA99FF", styles("-fx-background-color").get)
+  }
+
   /**
-   * Tests whether styles can be converted to a textual representation.
-   */
+    * Tests whether styles can be converted to a textual representation.
+    */
   @Test def testToExternalForm(): Unit = {
     val styles = Styles(StylesDefinition)
     checkExternalForm(styles, """-fx-font: 16px "Serif;Test";""",
@@ -75,12 +154,13 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Checks whether the external form of the given Styles object contains all
-   * the given styles definitions. Because toExternalForm() returns the
-   * styles in random order the test is a bit more tricky.
-   * @param styles the Styles object to check
-   * @param expDefs all the style definitions to check
-   */
+    * Checks whether the external form of the given Styles object contains all
+    * the given styles definitions. Because toExternalForm() returns the
+    * styles in random order the test is a bit more tricky.
+    *
+    * @param styles  the Styles object to check
+    * @param expDefs all the style definitions to check
+    */
   private def checkExternalForm(styles: Styles, expDefs: String*): Unit = {
     val ext = styles.toExternalForm
     for (sd <- expDefs) {
@@ -89,8 +169,8 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether style definitions can be removed.
-   */
+    * Tests whether style definitions can be removed.
+    */
   @Test def testRemoveStyles(): Unit = {
     val styles = Styles(StylesDefinition)
     styles -= "-fx-font"
@@ -100,8 +180,8 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether new styles can be added.
-   */
+    * Tests whether new styles can be added.
+    */
   @Test def testAddStyles(): Unit = {
     val styles = Styles()
     styles += ("-fx-new", "true")
@@ -109,8 +189,8 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether updateStyle() can be used to add/override styles.
-   */
+    * Tests whether updateStyle() can be used to add/override styles.
+    */
   @Test def testUpdateStyleAdd(): Unit = {
     val styles = Styles()
     styles += ("-fx-new", "perhaps")
@@ -121,8 +201,8 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether updateStyle() can be used to remove a style.
-   */
+    * Tests whether updateStyle() can be used to remove a style.
+    */
   @Test def testUpdateStyleRemove(): Unit = {
     val styles = Styles(StylesDefinition)
     styles.updateStyle("-fx-unknown", None)
@@ -130,8 +210,8 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests the string representation.
-   */
+    * Tests the string representation.
+    */
   @Test def testToString(): Unit = {
     val styles = Styles(StylesDefinition)
     val s = styles.toString()
@@ -139,10 +219,11 @@ class TestStyles extends JUnitSuite {
   }
 
   /**
-   * Tests whether parsing errors are handled.
-   */
-  @Test def testParseError(): Unit = {
+    * Tests whether a colon occurring in the value of a style is handled gracefully.
+    */
+  @Test def testKeySeparatorInValue(): Unit = {
     val styles = Styles("-fx-test: strange: value = ?")
-    assertTrue("Got styles", styles.styleKeys.isEmpty)
+
+    assertEquals("Wrong style", "strange: value = ?", styles("-fx-test").get)
   }
 }
